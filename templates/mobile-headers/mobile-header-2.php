3b54b0156e90cde253f1a607d6811d6c3ff2d5bc
fabrizio guespe <fguespe@gmail.com>
2018-10-01 15:18:38 -0300

    new

--- templates/mobile-headers/mobile-header-2.php
+++ templates/mobile-headers/mobile-header-2.php
@@ -1,15 +1,24 @@
 <?php 
 global $header_middle_mobile;
 ?>
+<style type="text/css">
+
+  .fw_header_middle.mobile i{
+    font-size:24px !important;
+}
+.fw_header_middle.mobile.navbar{
+    padding:0px;
+}
+
+
+</style>
 <div class="<?php echo esc_attr( $header_middle_mobile ); ?>  align-items-center ">
-    <div class="col-2 align-items-center">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button></div>
-    <div class="col-7 align-items-center text-center"><?php echo fastway_getLogo();?></div>
-		<div class="col-2 align-items-center"><?php echo fw_shoppingCart();?></div>
-    <div class="col-1 align-items-center">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-search"></i></button>
+      <div class="col-4 d-flex justify-content-around">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-search"></i></button>
       </div>
-      
+      <div class="col-5 text-center align-items-center"><?php echo fastway_getLogo();?></div>
+      <div class="col-3 text-right align-items-center"><?php echo fw_shoppingCart();?></div>
       <div class="collapse navbar-collapse" id="navbarsExample01">
         <nav class="navbar navbar-expand-md u-header__navbar">
           <?php wp_nav_menu(
@@ -30,7 +39,4 @@ global $header_middle_mobile;
            <div class="collapse navbar-collapse" id="navbarsExample02">
           <?php echo fw_search_form(3);?>
       </div>
-    
-      
 </div>
-</header>
