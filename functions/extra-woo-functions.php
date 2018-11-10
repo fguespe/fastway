862b32cd7e9ae0c0191c7b2b91b09e116a4a9f48
fabrizio guespe <fguespe@gmail.com>
2018-11-10 19:10:59 -0300

    new

--- functions/extra-woo-functions.php
+++ functions/extra-woo-functions.php
@@ -29,7 +29,7 @@ function fw_pngcheckout_short() {
 }
 if(fw_theme_mod("cart-steps")){
 add_action('woocommerce_before_cart', 'fw_pngcheckout_short');   
-add_action('woocommerce_before_checkout_form', 'fw_pngcheckout_short');  
+add_action('woocommerce_before_checkout_form', 'fw_pngcheckout_short',0);  
 }
 
 if(fw_theme_mod("checkout-minimal")){
