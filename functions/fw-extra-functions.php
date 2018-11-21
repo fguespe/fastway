6a722ddc755443f63dcc75d9175d0bca48c66b8f
fabrizio guespe <fguespe@gmail.com>
2018-11-20 22:33:41 -0300

    new

--- functions/fw-extra-functions.php
+++ functions/fw-extra-functions.php
@@ -4,16 +4,18 @@ function fw_mega_menu( $theme_location ) {
     if ( ($theme_location) && ($locations = get_nav_menu_locations()) && isset($locations[$theme_location]) ) {
         $menu = get_term( $locations[$theme_location], 'nav_menu' );
         $menu_items = wp_get_nav_menu_items($menu->term_id);
+        $megamenu=false;
+        $clasem="fwmenu1";
+        if($megamenu)$clasem="fwmenu2";
+
+        $menu_list  = '<nav id="menu-madre" class="'.$clasem.' navbar navbar-expand-md"><div class="collapse navbar-collapse" id=""><ul class="navbar-nav mr-auto">'."\n";
  
-        $menu_list  = '<nav id="menu-madre" class="navbar navbar-expand-md"><div class="collapse navbar-collapse" id=""><ul class="navbar-nav mr-auto">'."\n";
- 
-        $width=100;
-        $cols=4;
+        //$width=100;
+        if($megamenu)$cols=4;
+        else $cols=12;
+
         foreach( $menu_items as $menu_item ) {
             if( $menu_item->menu_item_parent == 0 ) {
-                //error_log($menu_item->title." ".$menu_item->menu_item_parent);
-                //error_log(print_r($menu_item,true));
-
                 $parent = $menu_item->ID;
                 $menu_array = array();
                         
@@ -22,26 +24,24 @@ function fw_mega_menu( $theme_location ) {
                         $bool = true;
                         $url=$submenu->url;
                         $title=$submenu->title;
-                        if($submenu->attr_title=="col"){
-                          error_log($submenu->title." ".$submenu->attr_title);
-                          $nuevoitem='<div class="col-md-'.$cols.'">';
-                        }
+                        if($submenu->attr_title=="col" && $megamenu)$nuevoitem='<div class="col-md-'.$cols.'">';
                         $nuevoitem .= '<li class="nav-item padre"><a class="nav-link" href="' . $url . '">' . $title . '</a></li>' ."\n";
                         $menu_array[] = $nuevoitem;
                         
                         //3er nivel
-                        $s_parent = $submenu->ID;
-                        foreach( $menu_items as $s_submenu ) {
-                            if( $s_submenu->menu_item_parent == $s_parent ) {
-                              $s_url=$s_submenu->url;
-                              $s_title=$s_submenu->title;
-                              $s_nuevoitem = '<li class="nav-item hijo"><a class="nav-link" href="' . $s_url . '">' . $s_title . '</a></li>' ."\n";
-                              $menu_array[] = $s_nuevoitem;
-                              error_log("------------".$s_submenu->title);
-                          } 
+                        if($megamenu){
+                            $s_parent = $submenu->ID;
+                            foreach( $menu_items as $s_submenu ) {
+                                if( $s_submenu->menu_item_parent == $s_parent ) {
+                                  $s_url=$s_submenu->url;
+                                  $s_title=$s_submenu->title;
+                                  $s_nuevoitem = '<li class="nav-item hijo"><a class="nav-link" href="' . $s_url . '">' . $s_title . '</a></li>' ."\n";
+                                  $menu_array[] = $s_nuevoitem;
+                                  error_log("------------".$s_submenu->title);
+                              } 
+                            }
+                            if($submenu->attr_title=="col" && $megamenu)$menu_array[] ='</div>';
                         }
-                        if($submenu->attr_title=="col")$menu_array[] ='</div>';
-                        
                         
                     }
                     
@@ -49,14 +49,15 @@ function fw_mega_menu( $theme_location ) {
 
                 if( $bool == true && count( $menu_array ) > 0 ) {
                      
-                    $menu_list .= '<li class="nav-item dropdown " style="position:static;">' ."\n";
+                    $menu_list .= '<li class="nav-item dropdown ">' ."\n";
                     $menu_list .= '<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">' . $menu_item->title . ' <span class="caret"></span></a>' ."\n";
-                     
-                    $menu_list .= '<ul class="submenu-madre" style="width:'.$width.'%;">' ."\n";
-                    $menu_list .= '<div class="row">';
+                    $clasemenu="submenu-madre"; 
+                
+                    $menu_list .= '<ul class="'.$clasemenu.'" style="width:'.$width.'%;">' ."\n";
+                    if($megamenu)$menu_list .= '<div class="row">';
                 
                     $menu_list .= implode( "\n", $menu_array );
-                    $menu_list .= '</div>';
+                    if($megamenu)$menu_list .= '</div>';
                 
                     $menu_list .= '</ul>' ."\n";
                      
--- templates/headers/header-9-2.php
+++ templates/headers/header-9-2.php
@@ -13,20 +13,7 @@
 <div class=" <?php echo esc_attr( $header_bottom ); ?> ">
   <div class="<?php echo esc_attr( $header_container ); ?> ">
       <div class="row d-flex justify-content-between">
-	       <nav class="js-mega-menu  navbar-expand-md u-header__navbar">
-          <?php wp_nav_menu(
-            array(
-              'theme_location'  => 'primary',
-              'container_class' => 'collapse navbar-collapse show align-items-center',
-              'container_id'    => 'navbarNavDropdown',
-              'menu_class'      => 'navbar-nav mr-auto',
-              'fallback_cb'     => '',
-              'menu_id'         => 'main-menu',
-              'depth'           => 2,
-              'walker'          => new fw_Navwalker(),
-            )
-          ); ?>
-        </nav>
+	       <?php fw_mega_menu("primary"); ?>
         <a class="mudamos" href="">
             <i class="fa fa-map-marker"></i>
             <strong>NOS MUDAMOS</strong>
