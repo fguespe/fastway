3997b4b207e9a81eba65a91365bd6147a844c06e
fabrizio guespe <fguespe@gmail.com>
2018-11-27 09:38:21 -0300

    new

--- functions.php
+++ functions.php
@@ -4,7 +4,8 @@ function fw_theme_mod( $name ) {
     //global $my_theme_defaults;
     if($name=="icons_style" && empty(get_theme_mod( $name)))return "fa";
     if($name=="icons_style" &&  get_theme_mod( $name) === "light")return "fal";
-    if($name=="icons_style" &&  get_theme_mod( $name) === "solid")return "fal";
+    if($name=="icons_style" &&  get_theme_mod( $name) === "solid")return "fas";
+    if($name=="icons_style" &&  get_theme_mod( $name) === "regular")return "fa";
     return get_theme_mod( $name);
 }
 function fw_getme_roles(){
