8f1e601b7afff4b218fa3e240d839b02a3d8da41
fabrizio guespe <fguespe@gmail.com>
2019-01-16 11:24:06 -0300

    new

--- templates/single-products/single-product-1.php
+++ templates/single-products/single-product-1.php
@@ -34,7 +34,35 @@ if(count($product->get_gallery_attachment_ids())>0)$clasecant='';
             }
 			remove_action('woocommerce_single_product_summary','woocommerce_template_single_excerpt',20);
 			do_action( 'woocommerce_single_product_summary' );
-            dynamic_sidebar('sp-sumary');
+			?>
+			<a target="_blank" data-toggle="" data-target="" class="fancybox btn-medios block btn-mobile t2 segura">
+	            <div class="row">
+	                <div class="col-2 text-left v-top txt-28"><i class="far fa-shield-alt"></i></div>
+	                <div class="col-10 text-left calcular-costo-envio">
+	                	<h4 class="">Compra Segura</h4>
+	                    <span class="">Garantía de Fabrica</span>
+	                </div>
+                </div>
+            </a>
+            <a target="_blank" data-toggle="" data-target="" class="fancybox btn-medios block btn-mobile t2 prueba">
+	            <div class="row">
+	                <div class="col-2 text-left v-top txt-28"><i class="fal fa-gift"></i></div>
+	                <div class="col-10 text-left calcular-costo-envio">
+	                	<h4 class="">Beneficio incluído</h4>
+	                    <span class="">10 días de prueba</span>
+	                </div>
+                </div>
+            </a>
+            <a target="_blank" data-toggle="modal" data-target="#modalMediosPago" class="fancybox btn-medios block btn-mobile pagos">
+	            <div class="row">
+	                <div class="col-2 text-left v-top txt-28"><i class="fal fa-credit-card"></i></div>
+	                <div class="col-10 text-left calcular-costo-envio">
+	                	<h4 class="">Ver cuotas y medios de pago</h4>
+	                    <span class="">(Ver promociones vigentes)</span>
+	                </div>
+	            </div>
+			</a>
+			<?
             echo getFinanciacion($product);
 			fw_share_redes();
 			?>
