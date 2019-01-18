967d4edaf1020db38a457c2ced910ebd1f803250
fabrizio guespe <fguespe@gmail.com>
2019-01-18 11:51:22 -0300

    new

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -8,31 +8,34 @@ if ( ! function_exists( 'fw_scripts' ) ) {
 			
 		wp_enqueue_script('jquery');
 		wp_enqueue_script( 'theme-scripts', get_template_directory_uri() . '/assets/js/fastway.min.js', array ( 'jquery' ),$version , true);
-		wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/assets/js/popper.min.js', array(),$version,  true );
-		wp_enqueue_script( 'bootstrap-scripts', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array(),$version,true );
+		wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/assets/js/popper.min.js', array(),'',  true );
+		wp_enqueue_script( 'bootstrap-scripts', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array(),'',true );
 		
 		if(is_plugin_active("woocommerce/woocommerce.php")){
-			wp_enqueue_script( 'swiper-script', get_template_directory_uri() . '/assets/js/swiper.min.js'  , array(),$version,  true );
-			wp_enqueue_style( 'swiper-styles', get_template_directory_uri() . '/assets/css/swiper.min.css', array(),$version,  true );
+			wp_enqueue_script( 'swiper-script', get_template_directory_uri() . '/assets/js/swiper.min.js'  );
+			wp_enqueue_style( 'swiper-styles', get_template_directory_uri() . '/assets/css/swiper.min.css');
 			if(is_product()){
-				wp_enqueue_style( 'fancybox-styles', get_template_directory_uri() . '/assets/css/jquery.fancybox.min.css', array(),$version,  true );
-				wp_enqueue_script( 'cycle-script', get_template_directory_uri() . '/assets/js/cycle.min.js' ,array(),$version,  true );
-				wp_enqueue_script( 'fancy-script', get_template_directory_uri() . '/assets/js/fancybox.min.js', array(),$version,  true );	
+				wp_enqueue_style( 'fancybox-styles', get_template_directory_uri() . '/assets/css/jquery.fancybox.min.css');
+				wp_enqueue_script( 'cycle-script', get_template_directory_uri() . '/assets/js/cycle.min.js' , array(),'',true );
+				wp_enqueue_script( 'fancy-script', get_template_directory_uri() . '/assets/js/fancybox.min.js');	
 			}
 		}
 		wp_localize_script( 'theme-scripts', 'ajaxurl', admin_url( 'admin-ajax.php' ),1 );	
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
+
+
+
 		//Fastway main css file, using SASS
-		wp_enqueue_style( 'theme-styles', get_template_directory_uri() . '/assets/css/theme.min.css', array(),$version,  true );
+		wp_enqueue_style( 'theme-styles', get_template_directory_uri() . '/assets/css/theme.min.css', false,$version);
 	}
 } 
 add_action( 'wp_enqueue_scripts', 'fw_scripts' );
 
 
 function admincustojs(){
-	$version=wp_get_theme("fastway")->get( 'Version' ) ;
-	wp_enqueue_script( 'custo-scripts', get_template_directory_uri() . '/assets/js/fw-customizer.js', array(),$version,  true );
+	wp_enqueue_script( 'custo-scripts', get_template_directory_uri() . '/assets/js/fw-customizer.js', array(), true );
+
 }
 add_action( 'customize_preview_init', 'admincustojs' );
