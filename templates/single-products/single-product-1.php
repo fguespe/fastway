77fdf2d35e3b3d7a79e5509976efd803818df1e9
fabrizio guespe <fguespe@gmail.com>
2019-01-20 18:16:41 -0300

    new

--- templates/single-products/single-product-1.php
+++ templates/single-products/single-product-1.php
@@ -13,31 +13,84 @@ if(count($product->get_gallery_attachment_ids())>0)$clasecant='';
 ?>
 
 <div class="container d-flex row px-0 mx-0">
-	<div class="gallery col-md-8 px-0 <?=$clasecant?>">
+  <div class="gallery col-md-8 px-0 <?=$clasecant?>">
          <div class="detalle-imagenListado  active">
             <div id="imagenListado" itemscope="" itemtype="http://schema.org/MediaObject" style="position: relative; overflow: hidden;">
-                <?fw_product_gallery($product);?>
-            </div>
-            <div id="mainNextIL" class="swiper-next"><i class="fa fa-angle-right"></i></div>
-            <div id="mainPrevIL" class="swiper-prev"><i class="fa fa-angle-left"></i></div>
-         </div>
+                <div class="swiper-single over-hidden relative">
+                    <div class="swiper-wrapper clear-ul">
 
-         <ul id="paginationIL" class="d-none d-sm-block"></ul>
-	</div>
- 	<div class="summary col-md-4">
-			<?
-			add_filter('woocommerce_get_price_html', 'fw_price_html1', 10, 2 );
-			do_action( 'woocommerce_single_product_summary' );
-			dynamic_sidebar('sp-sumary');
-			echo do_shortcode(stripslashes(htmlspecialchars_decode( fw_theme_mod('shop-single-product-html'))));
-			echo getFinanciacion($product);
-			echo fw_share_redes();
-			?>
-	</div>
+                        <?//fw_product_gallery($product);
+                        $fotos=$product->get_gallery_attachment_ids();
+                        array_push($fotos,intval(get_post_thumbnail_id( $product->id )));
+                        $fotos=array_reverse($fotos);
+                        $index=0;
+                        foreach ($fotos as $ids) {
+                            echo '<div class="swiper-slide">';
+                            $url=wp_get_attachment_url( $ids);
+                            echo '<a href='.$url.' data-fancybox="gallery" class="d-flex align-items-center" style="background-color: white; position: absolute; top: 0px; left: 0px; opacity: 1;">
+                                <img itemprop="image" src="'.$url.'" width=400 height="auto">
+                                <div class="lupaImg"><i class="fa fa-search-plus"></i></div>
+                            </a>';
+                            echo '</div>';
+                            $ul.= '
+                            <li class="">
+                                <a class="thumb" href="javascript:slideTo('.$index.')">
+                                    <img  src="'.$url.'" width="50">
+                                </a>
+                            </li>';
+                            $index++;
+                        }?>
+                    </div>
+                    
+                    <div class="swiper-prev swiper-single-prev"><i class="fa fa-angle-left"></i></div>
+                    <div class="swiper-next swiper-single-next"><i class="fa fa-angle-right"></i></div>
+                </div>
+            </div>
+        </div>
+        <style>
+.swiper-single{
+width:100%;
+height:100% ;
+}
+</style>
+<script>
+var swiper = new Swiper(".swiper-single", {
+    navigation: {
+        nextEl: ".swiper-single-next",
+        prevEl: ".swiper-single-prev",
+    },   
+    paginationClickable: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: { delay: 4500, },
+    autoplayDisableOnInteraction: true,
+    slidesPerView: 1
+});
+function slideTo(val){
+    swiper.slideTo(val+1, 500);
+}
+
+
+</script>
+       
+         <ul id="paginationIL" class="d-none d-sm-block"><?=$ul?></ul>
+  </div>
+    
+  <div class="summary col-md-4">
+      <?
+      add_filter('woocommerce_get_price_html', 'fw_price_html1', 10, 2 );
+      do_action( 'woocommerce_single_product_summary' );
+      dynamic_sidebar('sp-sumary');
+      echo do_shortcode(stripslashes(htmlspecialchars_decode( fw_theme_mod('shop-single-product-html'))));
+      echo getFinanciacion($product);
+      echo fw_share_redes();
+      ?>
+  </div>
 
 </div>
 <?php
-		
+    
 remove_action('woocommerce_after_single_product_summary','woocommerce_upsell_display',15);
 remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
 do_action( 'woocommerce_after_single_product_summary' );
