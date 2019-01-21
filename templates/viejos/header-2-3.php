4c5bd0dbb4ccdc555acf99e55b7892f081d67d3b
fabrizio guespe <fguespe@gmail.com>
2019-01-21 14:28:40 -0300

    new

---
+++ templates/viejos/header-2-3.php
@@ -0,0 +1,23 @@
+<?php global $header_container,$header_middle,$header_container; ?>
+<div class="<?php echo esc_attr( $header_middle ); ?>">
+  <div class="<?php echo esc_attr( $header_middle ); ?>">
+        <!-- Nav -->
+        <nav class="js-mega-menu navbar navbar-expand-md ">
+          <!-- Logo -->
+          <?php echo fastway_getLogo();?>
+          <?php wp_nav_menu(
+            array(
+                  'theme_location'  => 'primary',
+                  'container_class' => 'navbar-collapse py-0',
+                  'container_id'    => 'navBar',
+                  'menu_class'      => 'navbar-nav  mx-lg-auto',
+                  'fallback_cb'     => '',
+                  'menu_id'         => '',
+                  'walker'          => new fw_Navwalker('desktop-1'),
+                )
+            ); 
+            ?>
+        </nav>
+        <!-- End Nav -->
+      </div>
+</div>
---
+++ templates/viejos/header-9-2.php
@@ -0,0 +1,38 @@
+<?php global $header_container,$header_middle,$header_container,$header_bottom; ?>
+<div class=" <?php echo esc_attr( $header_middle ); ?>">
+    <div  class="<?php echo esc_attr( $header_container ); ?>">
+	    <div class="row d-flex justify-content-between">
+  		  <div class="logo"><?php echo fastway_getLogo();?></div>
+  			<div class="form-row align-items-center">
+          
+  				<?php fastway_getWidgetHeaderText();?>
+  			</div>
+	    </div>
+  </div>
+</div>
+<div class=" <?php echo esc_attr( $header_bottom ); ?> ">
+  <div class="<?php echo esc_attr( $header_container ); ?> ">
+      <div class="row d-flex justify-content-between">
+         <nav class="js-mega-menu  navbar-expand-md ">
+          <?php wp_nav_menu(
+            array(
+              'theme_location'  => 'primary',
+              'container_class' => 'collapse navbar-collapse show align-items-center',
+              'container_id'    => 'navbarNavDropdown',
+              'menu_class'      => 'navbar-nav mr-auto',
+              'fallback_cb'     => '',
+              'menu_id'         => 'main-menu',
+              'depth'           => 2,
+              'walker'          => new fw_Navwalker(),
+            )
+          ); ?>
+        </nav>
+        <a class="mudamos" href="">
+            <i class="fa fa-map-marker"></i>
+            <strong>NOS MUDAMOS</strong>
+            <span>Sarmiento 2608 </span>
+          </a>
+      </div>
+  </div>
+        
+</div>
