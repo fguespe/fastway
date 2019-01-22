dc081653707b6a3f746e20cd02b4015b75d30896
fabrizio guespe <fguespe@gmail.com>
2019-01-22 19:10:38 -0300

    new

--- templates/headers/header-1-1.php
+++ templates/headers/header-1-1.php
@@ -1,23 +1,11 @@
-<?php global $header_container,$header_middle,$header_container; ?>
+<?php global $header_container,$header_middle,$header_container,$header_bottom; ?>
 <div class="<?php echo esc_attr( $header_middle ); ?>">
-      <div  class="<?php echo esc_attr( $header_container ); ?>">
-        <nav class="navbar navbar-expand-md">
+    <div  class="<?php echo esc_attr( $header_container ); ?>">
+      <div  class="d-flex row justify-content-between <?php echo esc_attr( $header_container ); ?>">
           <?php echo fastway_getLogo();?>
-          <?php wp_nav_menu(
-          array(
-            'theme_location'  => 'primary',
-            'container_class' => 'collapse navbar-collapse show align-items-center',
-            'container_id'    => 'navbarNavDropdown',
-            'menu_class'      => 'navbar-nav ml-auto',
-            'fallback_cb'     => '',
-            'menu_id'         => 'main-menu',
-            'depth'           => 2,
-            'walker'          => new fw_Navwalker(),
-          )
-        ); 
-        if(is_plugin_active("woocommerce")) echo fw_shoppingCart();?>
-        </nav>
+          <?php fw_mega_menu("primary"); 
+          if(is_plugin_active("woocommerce")) echo fw_shoppingCart();?>
+          <?php fastway_getWidgetHeaderText();?>
       </div>
-      
-      
+      </div>
 </div>
