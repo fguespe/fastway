556052a688197205ad6b4f375bcc019a74e21692
fabrizio guespe <fguespe@gmail.com>
2019-01-23 13:31:47 -0300

    new

--- templates/headers/header-15-3.php
+++ templates/headers/header-15-3.php
@@ -17,3 +17,9 @@
       	</div>
   </div>
 </div>
+<?php global $header_container,$header_bottom; ?>
+<div class="<?php echo esc_attr( $header_bottom ); ?>">
+  <div class="<?php echo esc_attr( $header_container ); ?>">
+      <?php fw_menu("primary"); ?>
+  </div>
+</div>
