84554ca8aad1378edcb9c5040cd3b99e78016c52
fabrizio guespe <fguespe@gmail.com>
2019-01-24 18:58:30 -0300

    new

--- templates/headers/header-fw.php
+++ templates/headers/header-fw.php
@@ -1,16 +1,20 @@
-<?php global $header_container,$header_middle; ?>
+<?php global $header_container,$header_middle,$header_bottom; 
+$antdesp=explode("bottom_init",fw_header_html());
+$middleh=$antdesp[0];
+$afterh=$antdesp[1];
+?>
 <div class="<?php echo esc_attr( $header_middle ); ?>">
     <div  class="<?php echo esc_attr( $header_container ); ?>">
-	    <div class="d-flex row align-items-center">
-            <?php fw_header_html();?>
+	      <div class="d-flex row align-items-center">
+            <?php $middleh; ?>
       	</div>
     </div>
 </div>
-<?php global $header_container,$header_bottom; ?>
 <div class="<?php echo esc_attr( $header_bottom ); ?>">
   <div class="<?php echo esc_attr( $header_container ); ?>">
-      <?php fw_menu("primary"); ?>
-      <?php fw_header_html(); ?>
+        <div class="d-flex row align-items-center">
+            <?php $afterh; ?>
+      	</div>
   </div>
 </div>
 
