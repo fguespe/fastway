df9a840c452a31d15ac95ccb44516d04847d0503
fabrizio guespe <fguespe@gmail.com>
2019-01-24 18:52:37 -0300

    new

---
+++ templates/headers/header-fw.php
@@ -0,0 +1,26 @@
+<?php global $header_container,$header_middle; ?>
+<div class="<?php echo esc_attr( $header_middle ); ?>">
+    <div  class="<?php echo esc_attr( $header_container ); ?>">
+	    <div class="d-flex row align-items-center">
+            <?php fw_header_html();?>
+      	</div>
+    </div>
+</div>
+<?php global $header_container,$header_bottom; ?>
+<div class="<?php echo esc_attr( $header_bottom ); ?>">
+  <div class="<?php echo esc_attr( $header_container ); ?>">
+      <?php fw_menu("primary"); ?>
+      <?php fw_header_html(); ?>
+  </div>
+</div>
+
+<style>
+/*
+.fw_header_bottom.desktop #fw-menu .navbar-nav{
+		margin:0 auto;
+}
+.fw_header_bottom.desktop #fw-menu .navbar-nav{
+  width:100% !important;
+  justify-content:space-around !important;
+}*/
+</style>
