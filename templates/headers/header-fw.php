8e69faefe16f96fc6191ae0e81485ff840be50e9
fabrizio guespe <fguespe@gmail.com>
2019-01-26 13:01:24 -0300

    new

--- templates/headers/header-fw.php
+++ templates/headers/header-fw.php
@@ -20,6 +20,7 @@ $afterh=$antdesp[1];
 
 <style>
 /*
+centrar menu
 .fw_header.bottom.desktop #fw-menu .navbar-nav{
 		margin:0 auto;
 }
@@ -27,12 +28,15 @@ centrar logo o cualquier otro
 .logo{
 margin:0 auto !important;
 }
+
+
+#fw-menu{
+		width:100% !important;
+}
 .fw_header.bottom.desktop #fw-menu .navbar-nav{
   width:100% !important;
   justify-content:space-around !important;
 }
-#fw-menu{
-		width:100% !important;
-}
+
 */
 </style>
