961e1f3bda81c17d15c09ec1448590b6fd405cc8
fabrizio guespe <fguespe@gmail.com>
2019-02-01 10:40:43 -0300

    new

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -4,7 +4,7 @@ if ( ! function_exists( 'fw_scripts' ) ) {
 	function fw_scripts() {
 		$version=wp_get_theme("fastway")->get( 'Version' ) ;
 		wp_enqueue_style( 'bootstrap-styles', get_template_directory_uri() . '/assets/css/bootstrap.min.css');
-		//wp_enqueue_style( 'awesome-style', 'https://pro.fontawesome.com/releases/v5.5.0/css/all.css');
+		wp_enqueue_style( 'awesome-style', 'https://pro.fontawesome.com/releases/v5.5.0/css/all.css');
 			
 		wp_enqueue_script('jquery');
 		wp_enqueue_script( 'theme-scripts', get_template_directory_uri() . '/assets/js/fastway.min.js', array ( 'jquery' ),$version , true);
