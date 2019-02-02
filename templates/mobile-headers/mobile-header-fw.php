fc192a6abdd8deeb072dce13d9126ade1f9faa82
fabrizio guespe <fguespe@gmail.com>
2019-02-02 11:03:44 -0300

    new

--- templates/mobile-headers/mobile-header-fw.php
+++ templates/mobile-headers/mobile-header-fw.php
@@ -5,7 +5,7 @@ echo fw_header_html_mobile();
     <div class="menu-madre-mobile">
         <div class="sub-menu-mobile"> 
         <div class="telefono-header t1 txt-16 text-left">         
-          <a href="<?echo fw_company_data("phone",true)?>" rel="nofollow" title="Llamar" class="btn" style="color:white !important;background:#307BFF;font-size:20px !important;"><i class="fa fa-phone" style="color:white;" aria-hidden="true"></i> Llamar Ahora</a><br>
+          <a href="<?echo fw_company_data("phone",true)?>" rel="nofollow" title="Llamar" class="btn" style="color:white !important;background:#307BFF;font-size:20px !important;"><i class="fal fa-phone" style="color:white;" aria-hidden="true"></i> Llamar Ahora</a><br>
           <?if(!empty(fw_company_data("whatsapp"))){?>
           <a href="<?echo fw_company_data("whatsapp",true)?>" rel="nofollow" title="WhatsApp" class="btn" style="color:white !important;font-size:20px !important;background:#2AD348 !important;"><i class="fab fa-whatsapp" style="color:white !important;"></i> Consultar</a>
           <?}?>
