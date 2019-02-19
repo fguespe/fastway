99c050cbb9faf9c425b0940e5f73c05a07d2b5b9
fabrizio guespe <fguespe@gmail.com>
2019-02-19 16:15:13 -0300

    new

--- functions/fw-theme-options.php
+++ functions/fw-theme-options.php
@@ -530,7 +530,7 @@ Kirki::add_field( 'theme_config_id', array(
 	'transport'   => 'auto',
 	'output'      => array(
 		array(
-			'element' => '#fw_menu > .navbar-nav > li > a',
+			'element' => '.navbar-nav li > a',
 		),
 	),
 	'choices'  => array(
