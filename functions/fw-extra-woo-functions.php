738e172330b4273c3b43a3569ffa6e9fc4826e84
fabrizio guespe <fguespe@gmail.com>
2019-03-14 13:16:38 -0300

    new

--- functions/fw-extra-woo-functions.php
+++ functions/fw-extra-woo-functions.php
@@ -39,7 +39,7 @@ function fw_cat_image(){
     global $fw_woo_cat;
     $thumbnail_id = get_woocommerce_term_meta( $fw_woo_cat->term_id, 'thumbnail_id', true ); 
     $image = wp_get_attachment_url( $thumbnail_id ); 
-    return '<div class="thumbnail"><img src="'.$image.'" width="100%" height="auto" class="thumbnail"/></div>';
+    return '<div class="thumbnail"><img src="'.$image.'" width="100%" height="auto" /></div>';
 }
 
 
@@ -687,152 +687,159 @@ add_action( 'init', 'ess_custom_taxonomy_item', 0 );
 
 
 
-if ( ! class_exists( 'CT_TAX_META' ) ) {
+if ( ! class_exists( 'BRAND_THUMB' ) ) {
 
-    class CT_TAX_META {
-    
-      public function __construct() {
-        //
-      }
-    
-     /*
-      * Initialize the class and start calling our hooks and filters
-      * @since 1.0.0
-     */
-     public function init() {
-       add_action( 'brand_add_form_fields', array ( $this, 'add_category_image' ), 10, 2 );
-       add_action( 'created_brand', array ( $this, 'save_category_image' ), 10, 2 );
-       add_action( 'brand_edit_form_fields', array ( $this, 'update_category_image' ), 10, 2 );
-       add_action( 'edited_brand', array ( $this, 'updated_category_image' ), 10, 2 );
-       add_action( 'admin_footer', array ( $this, 'add_script' ) );
+  class BRAND_THUMB {
+  
+    public function __construct() {
+      //
+    }
+  
+   /*
+    * Initialize the class and start calling our hooks and filters
+    * @since 1.0.0
+   */
+   public function init() {
+     add_action( 'brand_add_form_fields', array ( $this, 'add_category_image' ), 10, 2 );
+     add_action( 'created_brand', array ( $this, 'save_category_image' ), 10, 2 );
+     add_action( 'brand_edit_form_fields', array ( $this, 'update_category_image' ), 10, 2 );
+     add_action( 'edited_brand', array ( $this, 'updated_category_image' ), 10, 2 );
+     add_action( 'admin_footer', array ( $this, 'add_script' ) );
+   }
+  
+   /*
+    * Add a form field in the new category page
+    * @since 1.0.0
+   */
+   public function add_category_image ( $taxonomy ) { ?>
+     <div class="form-field term-group">
+       <label for="thumbnail_id"><?php _e('Image', 'hero-theme'); ?></label>
+       <input type="hidden" id="thumbnail_id" name="thumbnail_id" class="custom_media_url" value="">
+       <div id="brand-thumb-wrapper"></div>
+       <p>
+         <input type="button" class="button button-secondary ct_tax_media_button" id="ct_tax_media_button" name="ct_tax_media_button" value="<?php _e( 'Add Image', 'hero-theme' ); ?>" />
+         <input type="button" class="button button-secondary ct_tax_media_remove" id="ct_tax_media_remove" name="ct_tax_media_remove" value="<?php _e( 'Remove Image', 'hero-theme' ); ?>" />
+      </p>
+     </div>
+   <?php
+   }
+  
+   /*
+    * Save the form field
+    * @since 1.0.0
+   */
+   public function save_category_image ( $term_id, $tt_id ) {
+     if( isset( $_POST['thumbnail_id'] ) && '' !== $_POST['thumbnail_id'] ){
+       $image = $_POST['thumbnail_id'];
+       add_term_meta( $term_id, 'thumbnail_id', $image, true );
      }
-    
-     /*
-      * Add a form field in the new category page
-      * @since 1.0.0
-     */
-     public function add_category_image ( $taxonomy ) { ?>
-       <div class="form-field term-group">
-         <label for="thumbnail_id"><?php _e('Image', 'hero-theme'); ?></label>
-         <input type="hidden" id="thumbnail_id" name="thumbnail_id" class="custom_media_url" value="">
-         <div id="category-image-wrapper"></div>
+   }
+  
+   /*
+    * Edit the form field
+    * @since 1.0.0
+   */
+   public function update_category_image ( $term, $taxonomy ) { ?>
+     <tr class="form-field term-group-wrap">
+       <th scope="row">
+         <label for="thumbnail_id"><?php _e( 'Image', 'hero-theme' ); ?></label>
+       </th>
+       <td>
+         <?php $image_id = get_term_meta ( $term -> term_id, 'thumbnail_id', true ); ?>
+         <input type="hidden" id="thumbnail_id" name="thumbnail_id" value="<?php echo $image_id; ?>">
+         <div id="brand-thumb-wrapper">
+           <?php if ( $image_id ) { ?>
+             <?php echo wp_get_attachment_image ( $image_id, 'thumbnail' ); ?>
+           <?php } ?>
+         </div>
          <p>
            <input type="button" class="button button-secondary ct_tax_media_button" id="ct_tax_media_button" name="ct_tax_media_button" value="<?php _e( 'Add Image', 'hero-theme' ); ?>" />
            <input type="button" class="button button-secondary ct_tax_media_remove" id="ct_tax_media_remove" name="ct_tax_media_remove" value="<?php _e( 'Remove Image', 'hero-theme' ); ?>" />
-        </p>
-       </div>
-     <?php
+         </p>
+       </td>
+     </tr>
+   <?php
+   }
+  
+  /*
+   * Update the form field value
+   * @since 1.0.0
+   */
+   public function updated_category_image ( $term_id, $tt_id ) {
+     if( isset( $_POST['thumbnail_id'] ) && '' !== $_POST['thumbnail_id'] ){
+       $image = $_POST['thumbnail_id'];
+       update_term_meta ( $term_id, 'thumbnail_id', $image );
+     } else {
+       update_term_meta ( $term_id, 'thumbnail_id', '' );
      }
-    
-     /*
-      * Save the form field
-      * @since 1.0.0
-     */
-     public function save_category_image ( $term_id, $tt_id ) {
-       if( isset( $_POST['thumbnail_id'] ) && '' !== $_POST['thumbnail_id'] ){
-         $image = $_POST['thumbnail_id'];
-         add_term_meta( $term_id, 'thumbnail_id', $image, true );
-       }
-     }
-    
-     /*
-      * Edit the form field
-      * @since 1.0.0
-     */
-     public function update_category_image ( $term, $taxonomy ) { ?>
-       <tr class="form-field term-group-wrap">
-         <th scope="row">
-           <label for="thumbnail_id"><?php _e( 'Image', 'hero-theme' ); ?></label>
-         </th>
-         <td>
-           <?php $image_id = get_term_meta ( $term -> term_id, 'thumbnail_id', true ); ?>
-           <input type="hidden" id="thumbnail_id" name="thumbnail_id" value="<?php echo $image_id; ?>">
-           <div id="category-image-wrapper">
-             <?php if ( $image_id ) { ?>
-               <?php echo wp_get_attachment_image ( $image_id, 'thumbnail' ); ?>
-             <?php } ?>
-           </div>
-           <p>
-             <input type="button" class="button button-secondary ct_tax_media_button" id="ct_tax_media_button" name="ct_tax_media_button" value="<?php _e( 'Add Image', 'hero-theme' ); ?>" />
-             <input type="button" class="button button-secondary ct_tax_media_remove" id="ct_tax_media_remove" name="ct_tax_media_remove" value="<?php _e( 'Remove Image', 'hero-theme' ); ?>" />
-           </p>
-         </td>
-       </tr>
-     <?php
-     }
-    
-    /*
-     * Update the form field value
-     * @since 1.0.0
-     */
-     public function updated_category_image ( $term_id, $tt_id ) {
-       if( isset( $_POST['thumbnail_id'] ) && '' !== $_POST['thumbnail_id'] ){
-         $image = $_POST['thumbnail_id'];
-         update_term_meta ( $term_id, 'thumbnail_id', $image );
-       } else {
-         update_term_meta ( $term_id, 'thumbnail_id', '' );
-       }
-     }
-    
-    /*
-     * Add script
-     * @since 1.0.0
-     */
-     public function add_script() { ?>
-       <script>
-         jQuery(document).ready( function($) {
-           function ct_media_upload(button_class) {
-             var _custom_media = true,
-             _orig_send_attachment = wp.media.editor.send.attachment;
-             jQuery('body').on('click', button_class, function(e) {
-               var button_id = '#'+jQuery(this).attr('id');
-               var send_attachment_bkp = wp.media.editor.send.attachment;
-               var button = jQuery(button_id);
-               _custom_media = true;
-               wp.media.editor.send.attachment = function(props, attachment){
-                 if ( _custom_media ) {
-                   jQuery('#thumbnail_id').val(attachment.id);
-                   jQuery('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
-                   jQuery('#category-image-wrapper .custom_media_image').attr('src',attachment.sizes.thumbnail.url).css('display','block');
-                 } else {
-                   return _orig_send_attachment.apply( button_id, [props, attachment] );
-                 }
-                }
-             wp.media.editor.open(button);
-             return false;
-           });
-         }
-         ct_media_upload('.ct_tax_media_button.button');
-         jQuery('body').on('click','.ct_tax_media_remove',function(){
-           jQuery('#thumbnail_id').val('');
-           jQuery('#category-image-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
+   }
+  
+  /*
+   * Add script
+   * @since 1.0.0
+   */
+   public function add_script() { ?>
+     <script>
+       jQuery(document).ready( function($) {
+         function ct_media_upload(button_class) {
+           var _custom_media = true,
+           _orig_send_attachment = wp.media.editor.send.attachment;
+           jQuery('body').on('click', button_class, function(e) {
+             var button_id = '#'+jQuery(this).attr('id');
+             var send_attachment_bkp = wp.media.editor.send.attachment;
+             var button = jQuery(button_id);
+             _custom_media = true;
+             wp.media.editor.send.attachment = function(props, attachment){
+               if ( _custom_media ) {
+                 console.log(attachment.url)
+                 jQuery('#thumbnail_id').val(attachment.id);
+                 jQuery('#brand-thumb-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
+                 var thumb='#'
+                 if(attachment.sizes.thumbnail !== undefined)thumb =attachment.sizes.thumbnail.url;
+                 else thumb=attachment.url;//si es menor que 150px
+                 jQuery('#brand-thumb-wrapper .custom_media_image').attr('src',thumb).css('display','block');
+               } else {
+                 return _orig_send_attachment.apply( button_id, [props, attachment] );
+               }
+              }
+           wp.media.editor.open(button);
+           return false;
          });
-         // Thanks: http://stackoverflow.com/questions/15281995/wordpress-create-category-ajax-response
-         jQuery(document).ajaxComplete(function(event, xhr, settings) {
-           var queryStringArr = settings.data.split('&');
-           if( $.inArray('action=add-tag', queryStringArr) !== -1 ){
-             var xml = xhr.responseXML;
-             $response = jQuery(xml).find('term_id').text();
-             if($response!=""){
-               // Clear the thumb image
-               jQuery('#category-image-wrapper').html('');
-             }
-           }
-         });
+       }
+       ct_media_upload('.ct_tax_media_button.button');
+       jQuery('body').on('click','.ct_tax_media_remove',function(){
+         jQuery('#thumbnail_id').val('');
+         jQuery('#brand-thumb-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
        });
-     </script>
-     <?php }
-    
-      }
-    
-$CT_TAX_META = new CT_TAX_META();
-$CT_TAX_META -> init();
+       // Thanks: http://stackoverflow.com/questions/15281995/wordpress-create-category-ajax-response
+       jQuery(document).ajaxComplete(function(event, xhr, settings) {
+         var queryStringArr = settings.data.split('&');
+         if( $.inArray('action=add-tag', queryStringArr) !== -1 ){
+           var xml = xhr.responseXML;
+           $response = jQuery(xml).find('term_id').text();
+           if($response!=""){
+             // Clear the thumb image
+             jQuery('#brand-thumb-wrapper').html('');
+           }
+         }
+       });
+     });
+   </script>
+   <?php }
+  
+    }
+  
+$BRAND_THUMB = new BRAND_THUMB();
+$BRAND_THUMB -> init();
 
 }
 function load_wp_media_files() {
 wp_enqueue_media();
 }
 add_action( 'admin_enqueue_scripts', 'load_wp_media_files' );
+
+
+
 
 
 
