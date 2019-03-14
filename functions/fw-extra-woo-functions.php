4f32abb9bb98791d0a394b22eefc5a76996237a6
fabrizio guespe <fguespe@gmail.com>
2019-03-14 09:04:17 -0300

    new

--- functions/fw-extra-woo-functions.php
+++ functions/fw-extra-woo-functions.php
@@ -811,11 +811,308 @@ if ( ! class_exists( 'CT_TAX_META' ) ) {
     
       }
     
-    $CT_TAX_META = new CT_TAX_META();
-    $CT_TAX_META -> init();
+$CT_TAX_META = new CT_TAX_META();
+$CT_TAX_META -> init();
+
+}
+function load_wp_media_files() {
+wp_enqueue_media();
+}
+add_action( 'admin_enqueue_scripts', 'load_wp_media_files' );
+
+
+
+
+
+
+
+
+
+
+if ( ! class_exists( 'CT_TAX_METABANNER' ) ) {
+
+    class CT_TAX_METABANNER {
     
-    }
-    function load_wp_media_files() {
-    wp_enqueue_media();
-    }
-    add_action( 'admin_enqueue_scripts', 'load_wp_media_files' );
+      public function __construct() {
+   
+      }
+    
+     public function init() {
+       add_action( 'brand_add_form_fields', array ( $this, 'add_category_image' ), 10, 2 );
+       add_action( 'created_brand', array ( $this, 'save_category_image' ), 10, 2 );
+       add_action( 'brand_edit_form_fields', array ( $this, 'update_category_image' ), 10, 2 );
+       add_action( 'edited_brand', array ( $this, 'updated_category_image' ), 10, 2 );
+       add_action( 'admin_footer', array ( $this, 'add_script' ) );
+     }
+    
+
+     public function add_category_image ( $taxonomy ) { ?>
+       <div class="form-field term-group">
+         <label for="banner_id"><?php _e('Image', 'hero-theme'); ?></label>
+         <input type="hidden" id="banner_id" name="banner_id" class="custom_media_url" value="">
+         <div id="brand-banner-wrapper"></div>
+         <p>
+           <input type="button" class="button button-secondary brand_banner_button" id="brand_banner_button" name="brand_banner_button" value="<?php _e( 'Add Image', 'hero-theme' ); ?>" />
+           <input type="button" class="button button-secondary brand_banner_remove" id="brand_banner_remove" name="brand_banner_remove" value="<?php _e( 'Remove Image', 'hero-theme' ); ?>" />
+        </p>
+       </div>
+     <?php
+     }
+    
+     /*
+      * Save the form field
+      * @since 1.0.0
+     */
+     public function save_category_image ( $term_id, $tt_id ) {
+       if( isset( $_POST['banner_id'] ) && '' !== $_POST['banner_id'] ){
+         $image = $_POST['banner_id'];
+         add_term_meta( $term_id, 'banner_id', $image, true );
+       }
+     }
+    
+     /*
+      * Edit the form field
+      * @since 1.0.0
+     */
+     public function update_category_image ( $term, $taxonomy ) { ?>
+       <tr class="form-field term-group-wrap">
+         <th scope="row">
+           <label for="banner_id"><?php _e( 'Banner', 'hero-theme' ); ?></label>
+         </th>
+         <td>
+           <?php $image_id = get_term_meta ( $term -> term_id, 'banner_id', true ); ?>
+           <input type="hidden" id="banner_id" name="banner_id" value="<?php echo $image_id; ?>">
+           <div id="brand-banner-wrapper">
+             <?php if ( $image_id ) { ?>
+               <?php echo wp_get_attachment_image ( $image_id, 'thumbnail' ); ?>
+             <?php } ?>
+           </div>
+           <p>
+             <input type="button" class="button button-secondary brand_banner_button" id="brand_banner_button" name="brand_banner_button" value="<?php _e( 'Add Image', 'hero-theme' ); ?>" />
+             <input type="button" class="button button-secondary brand_banner_remove" id="brand_banner_remove" name="brand_banner_remove" value="<?php _e( 'Remove Image', 'hero-theme' ); ?>" />
+           </p>
+         </td>
+       </tr>
+     <?php
+     }
+    
+    /*
+     * Update the form field value
+     * @since 1.0.0
+     */
+     public function updated_category_image ( $term_id, $tt_id ) {
+       if( isset( $_POST['banner_id'] ) && '' !== $_POST['banner_id'] ){
+         $image = $_POST['banner_id'];
+         update_term_meta ( $term_id, 'banner_id', $image );
+       } else {
+         update_term_meta ( $term_id, 'banner_id', '' );
+       }
+     }
+    
+    /*
+     * Add script
+     * @since 1.0.0
+     */
+     public function add_script() { ?>
+       <script>
+         jQuery(document).ready( function($) {
+           function ct_media_upload(button_class) {
+             var _custom_media = true,
+             _orig_send_attachment = wp.media.editor.send.attachment;
+             jQuery('body').on('click', button_class, function(e) {
+               var button_id = '#'+jQuery(this).attr('id');
+               var send_attachment_bkp = wp.media.editor.send.attachment;
+               var button = jQuery(button_id);
+               _custom_media = true;
+               wp.media.editor.send.attachment = function(props, attachment){
+                 if ( _custom_media ) {
+                   jQuery('#banner_id').val(attachment.id);
+                   jQuery('#brand-banner-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
+                   jQuery('#brand-banner-wrapper .custom_media_image').attr('src',attachment.sizes.thumbnail.url).css('display','block');
+                 } else {
+                   return _orig_send_attachment.apply( button_id, [props, attachment] );
+                 }
+                }
+             wp.media.editor.open(button);
+             return false;
+           });
+         }
+         ct_media_upload('.brand_banner_button.button');
+         jQuery('body').on('click','.brand_banner_remove',function(){
+           jQuery('#banner_id').val('');
+           jQuery('#brand-banner-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
+         });
+         // Thanks: http://stackoverflow.com/questions/15281995/wordpress-create-category-ajax-response
+         jQuery(document).ajaxComplete(function(event, xhr, settings) {
+           var queryStringArr = settings.data.split('&');
+           if( $.inArray('action=add-tag', queryStringArr) !== -1 ){
+             var xml = xhr.responseXML;
+             $response = jQuery(xml).find('term_id').text();
+             if($response!=""){
+               // Clear the thumb image
+               jQuery('#brand-banner-wrapper').html('');
+             }
+           }
+         });
+       });
+     </script>
+     <?php }
+    
+      }
+
+$CT_TAX_METABANNER = new CT_TAX_METABANNER();
+$CT_TAX_METABANNER -> init();
+
+}
+function load_wp_media_files2() {
+wp_enqueue_media();
+}
+add_action( 'admin_enqueue_scripts', 'load_wp_media_files2' );
+
+
+
+
+
+
+
+
+
+if ( ! class_exists( 'PRODUCT_CAT_BANNER' ) ) {
+
+    class PRODUCT_CAT_BANNER {
+    
+      public function __construct() {
+   
+      }
+    
+     public function init() {
+       add_action( 'product_cat_add_form_fields', array ( $this, 'add_category_image' ), 10, 2 );
+       add_action( 'created_product_cat', array ( $this, 'save_category_image' ), 10, 2 );
+       add_action( 'product_cat_edit_form_fields', array ( $this, 'update_category_image' ), 10, 2 );
+       add_action( 'edited_product_cat', array ( $this, 'updated_category_image' ), 10, 2 );
+       add_action( 'admin_footer', array ( $this, 'add_script' ) );
+     }
+    
+
+     public function add_category_image ( $taxonomy ) { ?>
+       <div class="form-field term-group">
+         <label for="banner_id"><?php _e('Image', 'hero-theme'); ?></label>
+         <input type="hidden" id="banner_id" name="banner_id" class="custom_media_url" value="">
+         <div id="product_cat-banner-wrapper"></div>
+         <p>
+           <input type="button" class="button button-secondary product_cat_banner_button" id="product_cat_banner_button" name="product_cat_banner_button" value="<?php _e( 'Add Image', 'hero-theme' ); ?>" />
+           <input type="button" class="button button-secondary product_cat_banner_remove" id="product_cat_banner_remove" name="product_cat_banner_remove" value="<?php _e( 'Remove Image', 'hero-theme' ); ?>" />
+        </p>
+       </div>
+     <?php
+     }
+    
+     /*
+      * Save the form field
+      * @since 1.0.0
+     */
+     public function save_category_image ( $term_id, $tt_id ) {
+       if( isset( $_POST['banner_id'] ) && '' !== $_POST['banner_id'] ){
+         $image = $_POST['banner_id'];
+         add_term_meta( $term_id, 'banner_id', $image, true );
+       }
+     }
+    
+     /*
+      * Edit the form field
+      * @since 1.0.0
+     */
+     public function update_category_image ( $term, $taxonomy ) { ?>
+       <tr class="form-field term-group-wrap">
+         <th scope="row">
+           <label for="banner_id"><?php _e( 'Banner', 'hero-theme' ); ?></label>
+         </th>
+         <td>
+           <?php $image_id = get_term_meta ( $term -> term_id, 'banner_id', true ); ?>
+           <input type="hidden" id="banner_id" name="banner_id" value="<?php echo $image_id; ?>">
+           <div id="product_cat-banner-wrapper">
+             <?php if ( $image_id ) { ?>
+               <?php echo wp_get_attachment_image ( $image_id, 'thumbnail' ); ?>
+             <?php } ?>
+           </div>
+           <p>
+             <input type="button" class="button button-secondary product_cat_banner_button" id="product_cat_banner_button" name="product_cat_banner_button" value="<?php _e( 'Add Image', 'hero-theme' ); ?>" />
+             <input type="button" class="button button-secondary product_cat_banner_remove" id="product_cat_banner_remove" name="product_cat_banner_remove" value="<?php _e( 'Remove Image', 'hero-theme' ); ?>" />
+           </p>
+         </td>
+       </tr>
+     <?php
+     }
+    
+    /*
+     * Update the form field value
+     * @since 1.0.0
+     */
+     public function updated_category_image ( $term_id, $tt_id ) {
+       if( isset( $_POST['banner_id'] ) && '' !== $_POST['banner_id'] ){
+         $image = $_POST['banner_id'];
+         update_term_meta ( $term_id, 'banner_id', $image );
+       } else {
+         update_term_meta ( $term_id, 'banner_id', '' );
+       }
+     }
+    
+    /*
+     * Add script
+     * @since 1.0.0
+     */
+     public function add_script() { ?>
+       <script>
+         jQuery(document).ready( function($) {
+           function ct_media_upload(button_class) {
+             var _custom_media = true,
+             _orig_send_attachment = wp.media.editor.send.attachment;
+             jQuery('body').on('click', button_class, function(e) {
+               var button_id = '#'+jQuery(this).attr('id');
+               var send_attachment_bkp = wp.media.editor.send.attachment;
+               var button = jQuery(button_id);
+               _custom_media = true;
+               wp.media.editor.send.attachment = function(props, attachment){
+                 if ( _custom_media ) {
+                   jQuery('#banner_id').val(attachment.id);
+                   jQuery('#product_cat-banner-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
+                   jQuery('#product_cat-banner-wrapper .custom_media_image').attr('src',attachment.sizes.thumbnail.url).css('display','block');
+                 } else {
+                   return _orig_send_attachment.apply( button_id, [props, attachment] );
+                 }
+                }
+             wp.media.editor.open(button);
+             return false;
+           });
+         }
+         ct_media_upload('.product_cat_banner_button.button');
+         jQuery('body').on('click','.product_cat_banner_remove',function(){
+           jQuery('#banner_id').val('');
+           jQuery('#product_cat-banner-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
+         });
+         // Thanks: http://stackoverflow.com/questions/15281995/wordpress-create-category-ajax-response
+         jQuery(document).ajaxComplete(function(event, xhr, settings) {
+           var queryStringArr = settings.data.split('&');
+           if( $.inArray('action=add-tag', queryStringArr) !== -1 ){
+             var xml = xhr.responseXML;
+             $response = jQuery(xml).find('term_id').text();
+             if($response!=""){
+               // Clear the thumb image
+               jQuery('#product_cat-banner-wrapper').html('');
+             }
+           }
+         });
+       });
+     </script>
+     <?php }
+    
+      }
+
+$PRODUCT_CAT_BANNER = new PRODUCT_CAT_BANNER();
+$PRODUCT_CAT_BANNER -> init();
+
+}
+function load_wp_media_files3() {
+wp_enqueue_media();
+}
+add_action( 'admin_enqueue_scripts', 'load_wp_media_files3' );
