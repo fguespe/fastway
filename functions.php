8e88e5b37c3a55c7077170199cc3d34121bb4702
fabrizio guespe <fguespe@gmail.com>
2019-04-12 16:02:46 -0300

    new

--- functions.php
+++ functions.php
@@ -163,13 +163,13 @@ if(is_plugin_active('kirki/kirki.php')){
     require get_template_directory() . '/functions/fw-theme-options.php';
 }
 
-//if(fw_theme_mod('ca-switch')){
+if(is_plugin_active('kirki/kirki.php')){
     require get_template_directory() . '/functions/client-area/client-area.php';
     add_filter('woocommerce_shop_manager_editable_roles', function($roles) {
         $roles[]='shop_manager';
         return array_unique($roles);
       });   
-//}
+}
 add_action( 'init', 'fw_login_dev_logo', 999 );
 function fw_login_dev_logo(){
 
