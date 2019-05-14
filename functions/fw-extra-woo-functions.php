a4989d5389e71a5abc0099dd96acb4918ce78638
fabrizio guespe <fguespe@gmail.com>
2019-05-14 18:51:59 -0300

    new

--- functions/fw-extra-woo-functions.php
+++ functions/fw-extra-woo-functions.php
@@ -1,5 +1,61 @@
 <?php
 
+/*LOOP FUNCTIONS*/
+add_shortcode('fw_loop_image', 'fw_loop_image');
+function fw_loop_image(){
+    global $product;
+    echo woocommerce_get_product_thumbnail();
+}
+add_shortcode('fw_loop_title', 'fw_loop_title');
+function fw_loop_title(){
+    global $product;
+    echo '<h2 class="product_title">'.$product->post->post_title.'</h2>';
+}
+add_shortcode('fw_loop_price', 'fw_loop_price');
+function fw_loop_price(){
+    global $product;
+    echo '<span class="price">'.fw_price_html1(null,$product).'</span>';
+}
+add_shortcode('fw_loop_cart', 'fw_loop_cart');
+function fw_loop_cart() {
+    if((fw_theme_mod("fw_purchases_visibility")==="logged" && !is_user_logged_in()) || fw_theme_mod("fw_purchases_visibility")==="hide")return;
+    global $product;
+    echo sprintf( '<a href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" data-quantity="%s" class="%s product_type_%s single_add_to_cart_button btn  btn-block %s">
+    <i class="fas fa-spinner fa-spin" style="display:none;"></i> %s</a>',
+            esc_url( $product->add_to_cart_url() ),
+            esc_attr( $product->get_id() ),
+            esc_attr( $product->get_sku() ),
+            esc_attr( isset( $quantity ) ? $quantity : 1 ),
+            $product->is_purchasable() && $product->is_in_stock() ? 'add_to_cart_button' : '',
+            esc_attr( $product->get_type() ),
+            $product->get_type() == 'simple' ? 'ajax_add_to_cart' : '',
+            esc_html( $product->add_to_cart_text() )
+        );
+}
+add_shortcode('fw_loop_meta', 'fw_loop_meta');
+function fw_loop_meta($atts = [], $content = null){
+    $atts = shortcode_atts(array('type' => '' ), $atts );
+    echo '<div class="meta">';
+    echo do_shortcode(stripslashes(htmlspecialchars_decode($content)));
+    echo '</div>';
+}
+
+
+add_shortcode('fw_loop_container', 'fw_loop_container');
+function fw_loop_container($atts = [], $content = null){
+    global $product;
+    echo '<a href="'.$product->get_permalink($product->id).'">';
+    echo do_shortcode(stripslashes(htmlspecialchars_decode($content)));
+    echo '</a>';
+
+}
+
+function woo_loop_code(){
+    echo do_shortcode(stripslashes(htmlspecialchars_decode( fw_theme_mod('woo_loop_code'))));
+}
+
+
+
 function display_facebook_element()
 {
 	?>
