6528c109b58075e6f787bcb897030b1f45999731
fabrizio guespe <fguespe@gmail.com>
2019-05-20 16:30:30 -0300

    new

--- functions/fw-extra-woo-functions.php
+++ functions/fw-extra-woo-functions.php
@@ -100,16 +100,11 @@ add_action("admin_menu", "add_theme_menu_item");
 
 
 
-
-
-
-
-if((get_option('fw_currency_conversion')  || get_option('fw_general_discount')) && !is_admin()){
+if(get_option('fw_currency_conversion')  && !is_admin()){
     
   // Utility function to change the prices with a multiplier (number)
   function get_price_multiplier() {
-    if(get_option('fw_currency_conversion'))$price+=floatval(get_option('fw_currency_conversion'));
-    if(get_option('fw_general_discount'))$price+=floatval(get_option('fw_general_discount'))/100;
+    $price+=floatval(get_option('fw_currency_conversion'));
     return $price; // x2 for testing
   }
 
@@ -139,6 +134,37 @@ if((get_option('fw_currency_conversion')  || get_option('fw_general_discount')) 
     $hash[] = get_price_multiplier();
     return $hash;
   }
+}
+
+
+if(fw_theme_mod('fw_general_discount')  && !is_admin()){
+    
+  // Utility function to change the prices with a multiplier (number)
+  function get_price_multiplier() {
+    $price=floatval(1-(fw_theme_mod('fw_general_discount')/100));
+    return $price; // x2 for testing
+  }
+
+  // Simple, grouped and external products
+ // add_filter('woocommerce_product_get_price', 'custom_price', 99, 2 );
+  add_filter('woocommerce_product_get_sale_price', 'custom_price', 99, 2 );
+  // Variations
+  add_filter('woocommerce_product_variation_get_sale_price', 'custom_price', 99, 2 );
+ // add_filter('woocommerce_product_variation_get_price', 'custom_price', 99, 2 );
+  function custom_price( $price, $product ) {
+    if($product->regular_price)return floatval($product->regular_price * get_price_multiplier());
+  }
+
+  // Variable (price range)
+  //add_filter('woocommerce_variation_prices_price', 'custom_variable_price', 99, 3 );
+  add_filter('woocommerce_variation_prices_sale_price', 'custom_variable_price', 99, 3 );
+  function custom_variable_price( $price, $variation, $product ) {
+    // Delete product cached price  (if needed)
+    // wc_delete_product_transients($variation->get_id());
+
+    if($product->regular_price)return floatval($product->regular_price * get_price_multiplier());
+  }
+
 }
 
 
