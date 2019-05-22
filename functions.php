d10fb63868525aa04817711847c486db5fa9e69b
fabrizio guespe <fguespe@gmail.com>
2019-05-22 10:11:18 -0300

    new

--- functions.php
+++ functions.php
@@ -1,4 +1,19 @@
 <?php
+// Function to change email address
+ 
+function wpb_sender_email( $original_email_address ) {
+    return 'avisos@altoweb.co';
+}
+ 
+// Function to change sender name
+function wpb_sender_name( $original_email_from ) {
+    return 'Avisos Altoweb';
+}
+ 
+// Hooking up our functions to WordPress filters 
+add_filter( 'wp_mail_from', 'wpb_sender_email' );
+add_filter( 'wp_mail_from_name', 'wpb_sender_name' );
+
 
 function fastway_get_stblock( $cats = array('all') ){
     $res_args = array();
