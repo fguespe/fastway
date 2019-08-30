0bce92e1e73d60d1c4ee4a018e0f7e689f6f9c3a
fabrizio guespe <fguespe@gmail.com>
2019-08-30 10:31:02 -0300

    new

--- functions/fw-extra-woo-functions.php
+++ functions/fw-extra-woo-functions.php
@@ -583,16 +583,20 @@ function fw_price_html1($price,$product){
         
         $regular_price=$product->regular_price;
         $sale_price=$product->sale_price;
+
+        //Festi
         if (function_exists('get_product_prices') && 
         in_array(fw_get_current_user_role(),fw_theme_mod('ca_roles_mayorista'))) {
-          
-          //Festi
           $prica=get_product_prices($product->get_id());
           if($prica[fw_get_current_user_role()]){//verifica el precio seteado
             $sale_price=$prica['salePrice'][fw_get_current_user_role()];
             $regular_price=$prica[fw_get_current_user_role()];
           }
+          //Sin festi
+        }else if(in_array(fw_get_current_user_role(),fw_theme_mod('ca_roles_mayorista'))) {
+          $regular_price=get_post_meta($product->id,'precio_'.fw_get_current_user_role(),true);
         }
+
         if(intval($regular_price) && intval($sale_price)){
         $percentage= round((( ( $regular_price - $sale_price ) / $regular_price ) * 100));  
         }else{
