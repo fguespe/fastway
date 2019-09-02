c7ccf9e3469df76ce382f9b20913c4d009a17dcf
fabrizio guespe <fguespe@gmail.com>
2019-09-02 10:28:26 -0300

    new

--- functions/fw-extra-woo-functions.php
+++ functions/fw-extra-woo-functions.php
@@ -652,27 +652,29 @@ function fw_price_html1($price,$product){
         $sale_price=$product->sale_price;
 
         //Festi
-/*
-        $roles=fw_theme_mod('ca_roles_mayorista');
-        $roles=explode(",",$roles);
-        if (function_exists('get_product_prices') && 
-        in_array(fw_get_current_user_role(),$roles)) {
-          $prica=get_product_prices($product->get_id());
-          if($prica[fw_get_current_user_role()]){//verifica el precio seteado
-            $sale_price=$prica['salePrice'][fw_get_current_user_role()];
-            $regular_price=$prica[fw_get_current_user_role()];
+        if(!empty(fw_theme_mod('ca_roles_mayorista'))){
+          $roles=fw_theme_mod('ca_roles_mayorista');
+          $roles=explode(",",$roles);
+          if (function_exists('get_product_prices') && 
+          in_array(fw_get_current_user_role(),$roles)) {
+            $prica=get_product_prices($product->get_id());
+            if($prica[fw_get_current_user_role()]){//verifica el precio seteado
+              $sale_price=$prica['salePrice'][fw_get_current_user_role()];
+              $regular_price=$prica[fw_get_current_user_role()];
+            }
+            //Sin festi
+          }else if(in_array(fw_get_current_user_role(),$roles)) {
+            $precio=get_post_meta($product->id,'precio_'.fw_get_current_user_role(),true);
+            if($precio)$regular_price=$precio;
           }
-          //Sin festi
-        }else if(in_array(fw_get_current_user_role(),$roles)) {
-          $precio=get_post_meta($product->id,'precio_'.fw_get_current_user_role(),true);
-          if($precio)$regular_price=$precio;
+  
+          if(intval($regular_price) && intval($sale_price)){
+            $percentage= round((( ( $regular_price - $sale_price ) / $regular_price ) * 100));  
+          }else{
+              $percentage=0;
+          }
         }
-
-        if(intval($regular_price) && intval($sale_price)){
-        $percentage= round((( ( $regular_price - $sale_price ) / $regular_price ) * 100));  
-        }else{
-            $percentage=0;
-        }*/
+        
     }
     if($product->is_on_sale()){
     return '<span class="fw_price price1">
