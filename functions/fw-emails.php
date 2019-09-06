545bdf819dd91ed27d48ab5d341b097eae2477c1
fabrizio guespe <fguespe@gmail.com>
2019-09-06 16:57:42 -0300

    new

--- functions/fw-emails.php
+++ functions/fw-emails.php
@@ -4,7 +4,35 @@
 add_filter( 'wp_mail_from', 'wpb_sender_email' );
 add_filter( 'wp_mail_from_name', 'wpb_sender_name' );
 
+// Redefine user notification function
+if ( !function_exists('wp_new_user_notification') ) {
+    function wp_new_user_notification( $user_id, $plaintext_pass = '' ) {
+        $user = new WP_User($user_id);
 
+        $user_login = stripslashes($user->user_login);
+        $user_email = stripslashes($user->user_email);
+
+        $message  = sprintf(__('New user registration on your blog %s:'), get_option('blogname')) . "\r\n\r\n";
+        $message .= sprintf(__('Username: %s'), $user_login) . "\r\n\r\n";
+        $message .= sprintf(__('E-mail: %s'), $user_email) . "\r\n";
+
+        @wp_mail(get_option('admin_email'), sprintf(__('[%s] New User Registration'), get_option('blogname')), $message);
+
+        if ( empty($plaintext_pass) )
+            return;
+
+        $message  = __('Hi there,') . "\r\n\r\n";
+        $message .= sprintf(__("Welcome to %s! Here's how to log in:"), get_option('blogname')) . "\r\n\r\n";
+        $message .= wp_login_url() . "\r\n";
+        $message .= sprintf(__('Username: %s'), $user_login) . "\r\n";
+        $message .= sprintf(__('Password: %s'), $plaintext_pass) . "\r\n\r\n";
+        $message .= sprintf(__('If you have any problems, please contact me at %s.'), get_option('admin_email')) . "\r\n\r\n";
+        $message .= __('Adios!');
+
+        wp_mail($user_email, sprintf(__('[%s] Your username and password'), get_option('blogname')), $message);
+        error_log('jeeje');
+    }
+}
 function wpb_sender_email( $original_email_address ) {
     return 'avisos@altoweb.co';
 }
@@ -24,7 +52,6 @@ add_filter('woocommerce_email_recipient_failed_order', 'email_orden_cancelada', 
 add_filter('woocommerce_email_recipient_cancelled_order', 'email_orden_fallida', 1, 2);
 
 //Emails
-
 add_filter( 'wpmu_welcome_user_notification', 'bbg_wpmu_welcome_user_notification', 10, 3 );
 add_filter( 'wpmu_signup_user_notification', 'kc_wpmu_signup_user_notification', 10, 4 );
 function bbg_wpmu_welcome_user_notification($user_id, $password, $meta = '') {
@@ -104,7 +131,7 @@ if(get_option("fw_altoweb_mailconfig")){
     //Admin general
     //update_option("new_admin_email",get_option("nubicommerce_destinos_mail"));
     
-    
+
     update_option("woocommerce_new_order_recipient",get_option("nubicommerce_destinos_mail"));
     update_option("woocommerce_cancelled_order_recipient",get_option("nubicommerce_destinos_mail"));
     update_option("woocommerce_failed_order_recipient",get_option("nubicommerce_destinos_mail"));
---
+++ woocommerce/emails/customer-new-account.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Customer new account email
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/customer-new-account.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce/Templates/Emails
+ * @version 3.7.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+do_action( 'woocommerce_email_header', $email_heading, $email ); 
+
+?>
+
+<p>Bienvenido a <?php echo esc_html( $blogname )?><br><br>
+Gracias por crear una cuenta en nuestra web. Tu nombre de usuario es<?php echo esc_html( $user_login ) ?><br>
+Podés acceder a tu cuenta para ver pedidos, cambiar tu contraseña y más en: <br>
+<?php make_clickable( esc_url( wc_get_page_permalink( 'myaccount' ) ) ); ?><br><br>
+<?php if ( 'yes' === get_option( 'woocommerce_registration_generate_password' ) && $password_generated ) : ?>
+Tu contraseña se generó automáticamente: <?php echo esc_html( $user_pass ); ?><br>
+<?php endif; ?>
+Pero podés cambiarla cuando quieras.<br><br>
+
+¡Te esperamos! ;-)</p>
+
+
+<?php
+if ( $additional_content ) {
+	echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
+}
+
+do_action( 'woocommerce_email_footer', $email );
