a0fae15e0be72cf5920aad39bc4e0fa4caf9a553
fabrizioguespe <fabrizioguespe@MacFabri.local>
2019-09-18 14:51:01 -0300

    new

--- functions/fw-extra-woo-functions.php
+++ functions/fw-extra-woo-functions.php
@@ -1623,7 +1623,7 @@ if( ! function_exists('xa_product_default_weight') ) {
  * @param array $rates Array of rates found for the package.
  * @return array
  */
-function my_hide_shipping_when_free_is_available( $rates ) {
+function fw_hide_shipping_when_free_is_available( $rates ) {
 	$free = array();
 	foreach ( $rates as $rate_id => $rate ) {
 		if ( 'free_shipping' === $rate->method_id ) {
@@ -1633,4 +1633,4 @@ function my_hide_shipping_when_free_is_available( $rates ) {
 	}
 	return ! empty( $free ) ? $free : $rates;
 }
-add_filter( 'woocommerce_package_rates', 'my_hide_shipping_when_free_is_available', 100 );
+add_filter( 'woocommerce_package_rates', 'fw_hide_shipping_when_free_is_available', 100 );
