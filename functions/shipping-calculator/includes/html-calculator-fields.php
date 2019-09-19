8167faa732baa9eeb8884cc48f51c586136f85f9
fabrizioguespe <fabrizioguespe@MacFabri.local>
2019-09-19 19:37:11 -0300

    new

--- functions/shipping-calculator/includes/html-calculator-fields.php
+++ functions/shipping-calculator/includes/html-calculator-fields.php
@@ -3,7 +3,7 @@
 	<b>Ingrese su codigo postal para calcular costo de envio:</b>
 	<i class="fad fa-shipping-fast" style="color:var(--second);"></i>
 	<input type="tel" id="wscp-postcode" autocomplete="off"  name="wscp-postcode" class="input-text text" style="width:70px" />
-	<input type="button" id="wscp-button" class="button wscp-button" value="Calcular" >
+	<input type="button" id="wscp-button" class="btn_mp_calc_shipping" value="Calcular" >
 	<input type="hidden" name="wscp-nonce" id="wscp-nonce" value="<?= wp_create_nonce( "wscp-nonce" ); ?>">
 	<div id="wscp-response"></div>
 	</div>
