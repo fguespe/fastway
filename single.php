7f2129cdc59f50533c356e8440fd48311e238902
fabrizioguespe <fabrizioguespe@MacFabri.local>
2019-09-24 11:20:19 -0300

    new

--- single.php
+++ single.php
@@ -9,7 +9,7 @@ get_header();
 $container   = fw_theme_mod('container-main');
 ?>
 
-<div class="wrapper" id="single-wrapper">
+<div class="wrapper blog_page" id="single-wrapper">
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
