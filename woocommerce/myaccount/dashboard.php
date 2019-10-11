7f67ef6f9e2455b1db97977f4d3b0a858745a660
fabrizioguespe <fabrizioguespe@MacFabri.local>
2019-10-11 20:14:18 -0300

    new

--- woocommerce/myaccount/dashboard.php
+++ woocommerce/myaccount/dashboard.php
@@ -27,7 +27,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <h4><?php echo fw_theme_mod('fw_welcome_msg')?></h4>
 
 <div class="botonesaccount d-flex align-items-center">
-<a href="">VER PRODUCTOS</a> <a href="">CERRAR SESIÓN</a> 
+<a href="<?php echo get_permalink( wc_get_page_id( 'shop' ) ); ?>">VER PRODUCTOS</a> <a href="<?php echo wp_logout_url( home_url())?>">CERRAR SESIÓN</a> 
 </div>
 
 <?php
