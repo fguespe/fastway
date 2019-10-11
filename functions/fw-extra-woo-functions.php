6cdc14c90c2ee0c3bce4e151d9af9d9dc259a707
fabrizioguespe <fabrizioguespe@MacFabri.local>
2019-10-11 17:49:43 -0300

    new

--- functions/fw-extra-woo-functions.php
+++ functions/fw-extra-woo-functions.php
@@ -271,7 +271,7 @@ if(fw_theme_mod('fw_currency_conversion')  && !is_admin()){
 if(fw_theme_mod('fw_general_discount')!='' /* && !is_admin()*/){
   // Utility function to change the prices with a multiplier (number)
   function get_price_multiplier() {
-    $price=round(1-(fw_theme_mod('fw_general_discount')/100));
+    $price=floatval(1-(fw_theme_mod('fw_general_discount')/100));
     return $price; // x2 for testing
   }
 
@@ -280,13 +280,13 @@ if(fw_theme_mod('fw_general_discount')!='' /* && !is_admin()*/){
   add_filter('woocommerce_product_variation_get_sale_price', 'custom_price', 99, 2 );
   add_filter('woocommerce_product_variation_get_price', 'custom_price', 99, 2 );
   function custom_price( $price, $product ) {
-    if($product->regular_price)return round($product->regular_price * get_price_multiplier());
+    if($product->regular_price)return floatval($product->regular_price * get_price_multiplier());
   }
 
   add_filter('woocommerce_variation_prices_price', 'custom_variable_price', 99, 3 );
   add_filter('woocommerce_variation_prices_sale_price', 'custom_variable_price', 99, 3 );
   function custom_variable_price( $price, $variation, $product ) {
-    if($product->regular_price)return round($product->regular_price * get_price_multiplier());
+    if($product->regular_price)return floatval($product->regular_price * get_price_multiplier());
   }
 
 }
