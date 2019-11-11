cb909bb1567d9ed65da58f58e99bb10b7b85e03c
fabrizioguespe <fabrizioguespe@MacFabri.local>
2019-11-11 11:28:04 -0300

    new

--- functions/fw-ajax-woo-functions.php
+++ functions/fw-ajax-woo-functions.php
@@ -1,16 +1,10 @@
 <?php
 
-add_shortcode('fw_loop_cart', 'fw_loop_cart_ajax');
-function fw_loop_cart_ajax() {
+add_shortcode('fw_cart_ajax', 'fw_cart_ajax');
+function fw_cart_ajax() {
     global $product;
     if(fw_check_hide_purchases())return;
-
-    $cant=isset( $quantity ) ? $quantity : 1;
-    $clase=$product->is_purchasable() && $product->is_in_stock() ? 'add_to_cart_button' : '';
-    $type=$product->get_type() == 'simple' ? 'ajax_add_to_cart' : '';
-
-    
-    echo '<a href="'.$product->add_to_cart_url().'" data-quantity="'.$cant.'" class="'.$type.' product_type_'.$type.' single_add_to_cart_button btn btn-block '.$type.'">'. esc_html( $product->add_to_cart_text() ).'</a>';
+    echo '<button onclick="addtocart('. $product->id.')" class="single_add_to_cart_button btn btn-block '.$type.'">'. esc_html( $product->add_to_cart_text() ).'</button>';
 }
 
 
@@ -18,7 +12,7 @@ function fw_loop_cart_ajax() {
 add_action('wp_ajax_nopriv_cart_remove_item', 'cart_remove_item');
 add_action('wp_ajax_cart_remove_item', 'cart_remove_item');
 function cart_remove_item(){
-  $key= $_GET['cart_item_key'];
+  $key=$_GET['cart_item_key'];
   WC()->cart->get_subtotal( )( $key );
   exit();
 }
@@ -30,7 +24,14 @@ function cart_get_subtotal(){
   echo json_encode($result);
   exit();
 }
-
+add_action('wp_ajax_nopriv_add_to_cart', 'fw_add_to_cart');
+add_action('wp_ajax_sum_add_to_cart', 'fw_add_to_cart');
+function fw_add_to_cart(){
+  global $woocommerce;
+  $id= $_GET['id'];
+  WC()->cart->add_to_cart( $id );
+  exit();
+}
 add_action('wp_ajax_nopriv_sum_cart_qty', 'sum_cart_qty');
 add_action('wp_ajax_sum_cart_qty', 'sum_cart_qty');
 function sum_cart_qty(){
