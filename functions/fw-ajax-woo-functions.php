77f98110e85e679f95218987068b79add6765986
fabrizioguespe <fabrizioguespe@MacFabri.local>
2019-11-12 14:12:42 -0300

    new

--- functions/fw-ajax-woo-functions.php
+++ functions/fw-ajax-woo-functions.php
@@ -111,6 +111,8 @@ function fw_get_js_cart(){
       $nombre = $_product->get_name();
       $cant=$cart_item['quantity'];
       $precio=price_array($_product);
+      error_log(print_r($precio,true));
+      $precio=end($precio);
       $total_line=$precio*$cant;
       $arr = array('nombre' => $nombre, 'precio'=> $precio, 'quantity' => $cart_item['quantity'], 'url' => $image_url, 'cart_item_key' => $cart_item_key, 'line_subtotal' => $total_line);
       array_push($carta,$arr);
@@ -124,6 +126,7 @@ function fw_get_js_cart(){
 function price_array($product){
     
     $price = $product->get_price_html();
+    error_log($price);
     $del = array('<span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>', '</span>','<del>','<ins>');
     $price = str_replace($del, '', $price);
     $price = str_replace('</del>', '|', $price);
@@ -131,9 +134,8 @@ function price_array($product){
     $price = str_replace('"', '', $price);
     $price = str_replace(',', '', $price);
     $price_arr = explode('|', $price);
-    
     $price_arr = array_filter($price_arr);
-    return $price;
+    return $price_arr;
 }
 
 ?>
