c910cedc4ca792c40b95f82ca4769939cc7d697f
fabrizioguespe <fabrizioguespe@MacFabri.local>
2019-12-16 14:56:32 -0300

    new

--- functions/fw-emails.php
+++ functions/fw-emails.php
@@ -12,24 +12,24 @@ if ( !function_exists('wp_new_user_notification') ) {
         $user_login = stripslashes($user->user_login);
         $user_email = stripslashes($user->user_email);
 
-        $message  = sprintf(__('New user registration on your blog %s:'), get_option('blogname')) . "\r\n\r\n";
+        $message  = sprintf(__('Nuevo registro en tu web %s:'), get_option('blogname')) . "\r\n\r\n";
         $message .= sprintf(__('Username: %s'), $user_login) . "\r\n\r\n";
         $message .= sprintf(__('E-mail: %s'), $user_email) . "\r\n";
 
-        @wp_mail(get_option('admin_email'), sprintf(__('[%s] New User Registration'), get_option('blogname')), $message);
+        @wp_mail(get_option('admin_email'), sprintf(__('[%s] Nuevo Registro'), get_option('blogname')), $message);
 
         if ( empty($plaintext_pass) )
             return;
 
         $message  = __('Hi there,') . "\r\n\r\n";
-        $message .= sprintf(__("Welcome to %s! Here's how to log in:"), get_option('blogname')) . "\r\n\r\n";
+        $message .= sprintf(__("Bienvenido a %s! Así podes entrar:"), get_option('blogname')) . "\r\n\r\n";
         $message .= wp_login_url() . "\r\n";
-        $message .= sprintf(__('Username: %s'), $user_login) . "\r\n";
-        $message .= sprintf(__('Password: %s'), $plaintext_pass) . "\r\n\r\n";
-        $message .= sprintf(__('If you have any problems, please contact me at %s.'), get_option('admin_email')) . "\r\n\r\n";
+        $message .= sprintf(__('Nombre de usuario: %s'), $user_login) . "\r\n";
+        $message .= sprintf(__('Contraseña: %s'), $plaintext_pass) . "\r\n\r\n";
+        $message .= sprintf(__('Si tenes alguna consulta, por favor, escribinos: %s.'), get_option('admin_email')) . "\r\n\r\n";
         $message .= __('Adios!');
 
-        wp_mail($user_email, sprintf(__('[%s] Your username and password'), get_option('blogname')), $message);
+        wp_mail($user_email, sprintf(__('[%s] Tu información de acceso'), get_option('blogname')), $message);
        
     }
 }
