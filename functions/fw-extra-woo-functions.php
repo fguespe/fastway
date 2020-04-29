9463e04b08e2f150dbd35a4714f2d6e660ade79a
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-04-29 17:00:48 -0300

    new

--- functions/fw-extra-woo-functions.php
+++ functions/fw-extra-woo-functions.php
@@ -86,10 +86,9 @@ function fw_container($atts = [], $content = null){
     echo '</div>';
 }
 add_shortcode('fw_short_desc', 'fw_short_desc');
-add_shortcode('fw_short_desc', 'fw_short_desc');
 function fw_short_desc(){
     global $product;
-    echo wpautop($product->post->post_excerpt) ;
+    echo '<div class="desc"">'.wpautop($product->post->post_excerpt).'</div>';
 }
 
 
