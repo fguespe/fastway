1fa437270245766b0a52615aa4e9f778f11b4b62
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-05-15 08:29:10 -0300

    new

--- functions/fw-extra-woo-functions.php
+++ functions/fw-extra-woo-functions.php
@@ -727,17 +727,17 @@ function fw_otherwoo_options(){
 
     if(!empty(fw_theme_mod("fw_neworder_email_header"))){
       add_action( 'woocommerce_email_before_order_table', 'fw_add_header_to_order', 20, 4 );
-    }/*
+    }
     if(!empty(fw_theme_mod("fw_neworder_email_footer"))){
       add_action( 'woocommerce_email_footer', 'fw_add_footer_to_email', 20, 4 );
-    }*/
+    }
     
 }
-/*
-function fw_add_footer_to_email( $order, $sent_to_admin, $plain_text, $email ) {
+
+function fw_add_footer_to_email( $email ) {
   echo fw_theme_mod("fw_neworder_email_footer");
 }
-*/
+
 function fw_add_header_to_order( $order, $sent_to_admin, $plain_text, $email ) {
     if ( $email->id == 'customer_processing_order' ) echo fw_theme_mod("fw_neworder_email_header");
 }
