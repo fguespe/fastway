cb048bb96a9ee347b69911117271ef0a5c60e79b
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-05-15 10:54:42 -0300

    new

--- woocommerce/cart/cart-shipping.php
+++ woocommerce/cart/cart-shipping.php
@@ -53,7 +53,7 @@ $calculator_text          = '';
 				$costo=$method->cost;
 				if($costo==0  && $id=='flat_rate')$costo='A cargo del cliente';
 				else if($costo==0  && $id=='free_shipping')$costo=fw_theme_mod('fw_shipping_free_label');
-				else $costo="$".$costo;
+				else $costo="$".$id;
 				$active=checked( $method->id, $chosen_method, false )?"active":""
 				?>	
 					<li for="shipping_method_0_<?=$id?><?=$instance?>" class="capsula shipping <?=$active;?>" data-radio="shipping_method_0_<?=$value?>" data-costo="<?=$costo?>" data-label="<?=$titulo?>" data-value="<?=$value?>" >
