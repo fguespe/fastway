44ad7284722cfc2bce4d7883a493cc0a6b381c5c
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-05-16 11:40:31 -0300

    new

--- functions/altoweb-plugin/altoweb-plugin.php
+++ functions/altoweb-plugin/altoweb-plugin.php
@@ -42,10 +42,10 @@ function after_xml_import_init_cate($import_id){
 
 if(fw_theme_mod('fw_action_init_mayorista')){
 
-   update_option('woocommerce_enable_guest_checkout','no');
-   update_option('woocommerce_enable_checkout_login_reminder','no');
-   update_option('woocommerce_enable_signup_and_login_from_checkout','no');
-   update_option('woocommerce_enable_myaccount_registration','no');
+   //update_option('woocommerce_enable_guest_checkout','no');
+   //update_option('woocommerce_enable_checkout_login_reminder','no');
+   //update_option('woocommerce_enable_signup_and_login_from_checkout','no');
+   //update_option('woocommerce_enable_myaccount_registration','no');
    set_theme_mod('fw_prices_visibility','logged');
    set_theme_mod('fw_purchases_visibility','logged');
    set_theme_mod('fw_action_init_mayorista',false);
@@ -62,6 +62,7 @@ if(fw_theme_mod('fw_action_woosettings')){
    update_option('shop_single_image_size','a:3:{s:5:"width";s:3:"500";s:6:"height";s:3:"500";s:4:"crop";i:1;}');
    update_option('woocommerce_enable_myaccount_registration','yes');
    update_option('woocommerce_enable_signup_and_login_from_checkout','yes');
+   update_option('woocommerce_enable_guest_checkout','yes');
    update_option('woocommerce_enable_checkout_login_reminder','yes');
    update_option('woocommerce_hide_out_of_stock_items','yes');
    update_option('woocommerce_registration_generate_username','yes');
