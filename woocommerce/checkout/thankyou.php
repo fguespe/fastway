c64d31bf6957d5f5277ffa61e885360386c87e17
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-05-16 11:59:10 -0300

    new

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -42,7 +42,7 @@ defined( 'ABSPATH' ) || exit;
 				<p><?=fw_theme_mod('fw_label_checkout_thank_1')?> #<?=$order->get_order_number()?></p>
 				<span><?=fw_theme_mod('fw_label_checkout_thank_2')?> <b><?=$order->get_billing_email()?></b> <?=fw_theme_mod('fw_label_checkout_thank_3')?></span>
 
-				<p ckass="mt20"><p><?=fw_theme_mod('checkout-msg')?></p>
+				<p class="mt20"><p><?=fw_theme_mod('checkout-msg')?></p>
 				<?php do_action( 'woocommerce_thankyou_' . $order->get_payment_method(), $order->get_id() ); ?><!--por si hay algo intersante para haer en el futuro....->
 
 				<a class="seguir" href="/"><?=fw_theme_mod('fw_label_checkout_thank_4')?></a>
