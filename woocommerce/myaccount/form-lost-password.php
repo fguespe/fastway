19ade3a44eda93378cdf01579dac2454c896ef2f
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-06-06 17:48:10 -0300

    new

--- woocommerce/myaccount/form-lost-password.php
+++ woocommerce/myaccount/form-lost-password.php
@@ -27,7 +27,7 @@ wc_print_notices(); ?>
 	<p><?php echo apply_filters( 'woocommerce_lost_password_message', __( 'Perdiste tu contraseńa? Por favor ingresá tu nombre de usuario o email y recibirás un link para reiniciarla.', 'woocommerce' ) ); ?></p>
 
 	<p class="woocommerce-FormRow woocommerce-FormRow--first form-row form-row-first">
-		<label for="user_login"><?php _e( 'Usuario o email', 'fastway' ); ?></label>
+		<label for="user_login"><?php _e( 'Usuario o email', 'woocommerce' ); ?></label>
 		<input class="woocommerce-Input woocommerce-Input--text input-text form-control" type="text" name="user_login" id="user_login" />
 	</p>
 
@@ -37,7 +37,7 @@ wc_print_notices(); ?>
 
 	<p class="woocommerce-form-row form-row">
 		<input type="hidden" name="wc_reset_password" value="true" />
-		<input type="submit" class="btn " value="<?php esc_attr_e( 'Resetear Contraseña', 'fastway' ); ?>" />
+		<input type="submit" class="btn " value="<?php esc_attr_e( 'Resetear Contraseña', 'woocommerce' ); ?>" />
 	</p>
 
 	<?php wp_nonce_field( 'lost_password' ); ?>
