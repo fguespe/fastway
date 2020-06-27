fe2282771352473082ea5861c008b6cf15c5e953
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-06-27 17:35:58 -0300

    new

--- functions/meli/callback.php
+++ functions/meli/callback.php
@@ -6,5 +6,6 @@ header("HTTP/1.1 200 OK");
 error_log('entro una noti de ML ');
 if($_POST)error_log(print_r($_POST,true));
 
+error_log('input... ');
 $notifications=file_get_contents("php://input");
 error_log(print_r($notifications,true));
