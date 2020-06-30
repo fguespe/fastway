40dde313dc7f22626a4c92d421813e816956695b
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-06-30 14:34:23 -0300

    new

--- functions/meli/callback.php
+++ functions/meli/callback.php
@@ -5,9 +5,13 @@ header("HTTP/1.1 200 OK");
 
 $notifications=file_get_contents("php://input");
 error_log('jeje');
-if($notifications && isset($notifications['resource'])){
+if($notifications){
     error_log('jaja');
     error_log(print_r($notifications,true));
     $order_id=explode($notifications['resource'],'/')[1];
     error_log('order_id: '.$order_id);
+}
+if($_POST){
+    error_log("Es post");
+    error_log(print_r($_POST,true));
 }
