e4e1ab7cf6af450d4d6c35dff96f8155607930a4
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-07-21 12:29:49 -0300

    new

--- functions/meli/fw-ml.php
+++ functions/meli/fw-ml.php
@@ -1,8 +1,8 @@
 <?php
 
 if(fw_theme_mod('fw_ml_stock_web_a_ml')){
-  add_action('woocommerce_checkout_order_processed', 'fw_ml_update_stock', 10, 1);
-  if(isLocalhost())add_action('woocommerce_thankyou', 'fw_ml_update_stock', 10, 1);
+  //esata corre antes!! add_action('woocommerce_checkout_order_processed', 'fw_ml_update_stock', 10, 1);
+  add_action('woocommerce_thankyou', 'fw_ml_update_stock', 10, 1);
 }
 function fw_ml_update_stock( $order_id ) {
     if ( ! $order_id )return;
