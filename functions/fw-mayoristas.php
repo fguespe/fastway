df2ff2eea0c7e8ca89f43779ef68a717d66b5222
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-08-13 13:30:31 -0300

    new

--- functions/fw-mayoristas.php
+++ functions/fw-mayoristas.php
@@ -53,7 +53,7 @@ if(fw_theme_mod('fw_client_admin_columnaprecio')){
       if ( $column == strtolower($nombre) ) {
         $valor=get_post_meta( $postid, 'festiUserRolePrices', true );
         if(is_super_admin()){
-          echo $valor;
+          echo print_r($valor);
         }else{
           echo '$'.parser_mayorista($valor);
         }
