545b0ea00b8c84aaaf275fd828be0f1924135d0d
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-09-08 13:41:15 -0300

    new

--- functions/meli/callback.php
+++ functions/meli/callback.php
@@ -1,8 +1,7 @@
 <?php
 $path = preg_replace('/wp-content.*$/','',__DIR__);require_once($path."/wp-load.php");
 header("HTTP/1.1 200 OK");
-custom_logs("Inical=3");
-
+custom_logs("Inical4");
 function custom_logs($message) { 
   if(is_array($message)) { 
       $message = json_encode($message); 
