c69f66849783c6d0d233207fa964f7d4578ecb19
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-09-07 22:07:59 -0300

    new

--- global-templates/modals.php
+++ global-templates/modals.php
@@ -465,7 +465,7 @@ jQuery( ".fw_variations select" ).change(function() {
             }else {
                 console.log('vara2',vara['is_in_stock'],vara['is_purchasable'],vara['backorders_allowed'])
                 jQuery('.fw_add_to_cart_button').prop("disabled",true)
-                if(jQuery('#mini_label_stock').length==0)jQuery('.fw_add_to_cart_button').after('<small id="mini_label_stock" style="text-align:center !important;margin:0 auto;display:block;"><?=fw_theme_mod('out-of-stock-text')?></small>')
+                jQuery('.fw_add_to_cart_button').after('<small id="mini_label_stock" style="text-align:center !important;margin:0 auto;display:block;"><?=fw_theme_mod('out-of-stock-text')?></small>')
             }
         }
     );
