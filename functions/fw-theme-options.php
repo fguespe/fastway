76e383f345593f5cb61ef3a9a67e0cddc172520a
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-09-21 08:19:45 -0300

    new

--- functions/fw-theme-options.php
+++ functions/fw-theme-options.php
@@ -1853,7 +1853,7 @@ Kirki::add_field( 'theme_config_id', array(
 	'type'        => 'color',
 	'settings'    => 'opt-color-topheader',
 	'label'       => __( 'Top Header ', 'fastway' ),
-	'section'     => 'section_colors',
+	'section'     => 'section_colorss',
 	'default'     => '#fff',
 	'choices'     => array(
 		'alpha' => true,
@@ -1876,7 +1876,7 @@ Kirki::add_field( 'theme_config_id', array(
 	'type'        => 'color',
 	'settings'    => 'opt-color-middheader',
 	'label'       => __( 'Middle Header ', 'fastway' ),
-	'section'     => 'section_colors',
+	'section'     => 'section_colorss',
 	'default'     => '#fff',
 	'choices'     => array(
 		'alpha' => true,
@@ -1898,7 +1898,7 @@ Kirki::add_field( 'theme_config_id', array(
 	'type'        => 'color',
 	'settings'    => 'color-sticky-header',
 	'label'       => __( 'Header Sticky Color ', 'fastway' ),
-	'section'     => 'section_colors',
+	'section'     => 'section_colorss',
 	'default'     => '#fff',
 	'description'=>' var(--sticky-header);',
 	'transport'   => 'auto',
@@ -1917,7 +1917,7 @@ Kirki::add_field( 'theme_config_id', array(
 	'type'        => 'color',
 	'settings'    => 'opt-color-bottheader',
 	'label'       => __( 'Bottom Header ', 'fastway' ),
-	'section'     => 'section_colors',
+	'section'     => 'section_colorss',
 	'default'     => '#fff',
 	'choices'     => array(
 		'alpha' => true,
