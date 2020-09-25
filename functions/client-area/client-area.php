5efbe28b52b5a4361b40d5ae945571d04d54ec64
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-09-25 10:01:18 -0300

    new

--- functions/client-area/client-area.php
+++ functions/client-area/client-area.php
@@ -642,15 +642,6 @@ function custom_admin_js() {
 		  lastName: "'.$nombre.'",                // user’s last name,
 		  email: "'.$mail.'",     
 		});
-		window.fcWidget.user.setProperties({
-			firstName: "Pepe",
-			lastName: "Pepe",
-			email: "f@altoweb.co",
-			phone: "None", 
-			phoneCountryCode: "+54",  
-			plan: "Ecommerce", 
-			status: "Active", 
-		});
 	  }
 	  function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"freshchat-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
 	</script>';
