ead6df5c289a816e65f721d8874f99a275dfa6e4
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-10-20 18:37:43 -0300

    new

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -540,6 +540,7 @@ function resetShippingFake(){
 jQuery(document).on('updated_checkout', function(){
   //updateEnvioGratisME();
   setTodopago();
+  if(paso<2)resetShippingFake();
   //setEpostnet();
   shippingGroups();
 	if(envioSeleccionado>0)jQuery('.shipping-total').attr("style", "display: table-row")
