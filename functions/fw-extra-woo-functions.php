9707e634137c3b4ee5c43ed4c0b8b81b4c8c613c
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-10-31 11:08:18 -0300

    new

--- functions/fw-extra-woo-functions.php
+++ functions/fw-extra-woo-functions.php
@@ -81,7 +81,6 @@ function fw_cuotas($atts = []){
   if($atts['cant']=='general')$atts['cant']=fw_theme_mod('fw_cuotas_general');
   $cuotas=floatval($atts['cant']);
   $precio=floatval($product->get_sale_price());
-  echo $precio;
   if( $product->is_type('variable') ){
     $precio = floatval($product->get_variation_regular_price( 'min', true ));
     $sale_price = floatval($product->get_variation_sale_price( 'min', true ));
@@ -90,6 +89,7 @@ function fw_cuotas($atts = []){
     if($sale_price>0)$precio= $sale_price;
   }
 
+  echo $precio;
   
   $precio=round($precio/$cuotas);
   if($precio>0)echo '<span class="cuota_text '.$atts['class'].'"><i class="fad fa-credit-card"></i> '.$cuotas.' cuotas de $'.$precio.'</span>';
