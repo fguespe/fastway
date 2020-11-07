19392b323bb5b8debd132c1c48d7db1f84dfd3a3
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-11-07 12:16:06 -0300

    new

--- woocommerce/single-product-reviews.php
+++ woocommerce/single-product-reviews.php
@@ -30,7 +30,13 @@ $average      = $product->get_average_rating();
 
 	<?php if ( have_comments() ) : ?>
 		<ol class="comment-list">
-			<?php wp_list_comments( apply_filters( 'woocommerce_product_review_list_args', array( 'callback' => 'woocommerce_comments' ) ) ); ?>
+			
+		<?php
+			wp_list_comments( array(
+				'style'      => 'ol',
+				'short_ping' => true,
+			) );
+			?>
 		</ol>
 
 		<?php
