0bae3b1865ac0b0096e4e04221bf8e0e1f9d23eb
fabrizioguespe <fabrizioguespe@MacFabri.local>
2020-12-11 14:15:23 -0300

    new

--- functions.php
+++ functions.php
@@ -516,7 +516,10 @@ function mc_edit_permission_check() {
 }
 add_filter( 'admin_head', 'mc_edit_permission_check', 1, 4 );
 
-
+function fw_precio_vacio($x) {
+  if(empty($x))return 0;
+  return $x;
+}
 
 
 ?>
