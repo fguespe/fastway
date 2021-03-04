186cfb30341e3b0b669e60a4d57f023c3cb6a6a0
fabrizioguespe <fabrizioguespe@MacFabri.local>
2021-03-04 11:36:53 -0300

    new

--- functions/custom-post-types/fw-events.php
+++ functions/custom-post-types/fw-events.php
@@ -348,7 +348,8 @@ add_shortcode('fw_event_url', 'fw_event_url');
 function fw_event_url(){
   global $fw_loop_event;
   if(function_exists('get_field'))echo '<a href="'.get_field('link',$fw_loop_event->ID).'" class="vermas" target="_blank" >'.get_field('btn_label',$fw_loop_event->ID).'</a>';
-  if(function_exists('get_field') && get_field('file',$fw_loop_event->ID))echo '<a style="margin-left:6px;color:var(--main);" href="'.get_field('file',$fw_loop_event->ID).'" class="" target="_blank" ><i class="fal fa-download"></i> Download</a>';
+  
+  if(function_exists('get_field') && get_field('file',$fw_loop_event->ID))echo '<a style="margin-left:6px;color:var(--main);" href="'.get_field('file',$fw_loop_event->ID)['url'].'" class="" target="_blank" ><i class="fal fa-download"></i> Download</a>';
 }
 
 
