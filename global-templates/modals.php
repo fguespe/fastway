99a5031df3cb0cfdc68040c8004662db7408c872
fabrizioguespe <fabrizioguespe@MacFabri.local>
2021-05-10 11:50:48 -0300

    new

--- global-templates/modals.php
+++ global-templates/modals.php
@@ -124,7 +124,7 @@
                                 <label class="control-label">Cuotas</label>
                                 <select id="cuotastp" class="dropdown-toggle bs-placeholder btn btn-main" onchange="calcular();">
                                 <?php 
-                                foreach(explode( ',',fw_theme_mod('fw_cuotas_todopago_calculador' ) as $cuota) echo '<option value="'.$cuota.'">'.$cuota.'</option>';
+                                foreach(explode( ',',fw_theme_mod('fw_cuotas_todopago_calculador' )) as $cuota) echo '<option value="'.$cuota.'">'.$cuota.'</option>';
                                 ?>
                                 </select>
                             </div>
