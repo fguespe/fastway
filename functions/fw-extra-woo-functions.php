54b413f408dd6808be78f2d12a050117c040c2f3
fabrizioguespe <fabrizioguespe@MacFabri.local>
2021-09-08 12:04:48 -0300

    new

--- functions/fw-extra-woo-functions.php
+++ functions/fw-extra-woo-functions.php
@@ -18,8 +18,8 @@ function fw_single_container($atts = [], $content = null){
 
 add_shortcode('fw_stock_label', 'fw_stock_label');
 function fw_stock_label(){
-    if($product->get_stock_quantity()<=1000)return "Disponibles: ".$product->get_stock_quantity();
-    else return "Disponibles: "."+ 1000";
+    if($product->get_stock_quantity()<=1000)echo "Disponibles: ".$product->get_stock_quantity();
+    else echo "Disponibles: "."+ 1000";
 }
 
 
