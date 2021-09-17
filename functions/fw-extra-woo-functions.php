6db859f3f68ce764b9a473b0bcc0d4899dee542e
fabrizioguespe <fabrizioguespe@MacFabri.local>
2021-09-17 12:50:32 -0300

    new

--- functions/fw-extra-woo-functions.php
+++ functions/fw-extra-woo-functions.php
@@ -426,7 +426,7 @@ add_shortcode('fw_loop_image', 'fw_loop_image');
 function fw_loop_image(){
     global $product;
     $image=woocommerce_get_product_thumbnail();
-    if(empty($image))$image="<img url='"."https://www.provetel.com.ar/wp-content/uploads/sites/12/2021/07/logo.png"."'/>";
+    if(empty($image))$image='<img width="300" height="300" src="/wp-content/uploads/sites/12/woocommerce-placeholder.png" class="woocommerce-placeholder wp-post-image" alt="Marcador" loading="lazy">';
     echo '<div class="loopimg_container">'.$image.'</div>';
 }
 
