15b95681c35a0248b02526c97765d9c2e595ac68
fabrizioguespe <fabrizioguespe@MacFabri.local>
2021-09-21 17:18:47 -0300

    new

--- functions/emails/fw-emails.php
+++ functions/emails/fw-emails.php
@@ -142,12 +142,6 @@ function fw_parse_mail($tipo,$order, $sent_to_admin=false, $plain_text=false,$em
   echo wp_kses_post( wpautop( wptexturize($html)));
 }
 
-function kia_display_email_order_user_meta( $order, $sent_to_admin, $plain_text ) {
-    $user_id = $order->customer_user;
-    echo "CUIT:".$user_id;
-   // if( ! empty( $user_id ) && ( $data = get_user_meta( $user_id, 'billing_dni', true ) ) != '' ) echo $data;
-}
-add_action('woocommerce_email_customer_details', 'kia_display_email_order_user_meta', 30, 3 );
 
 function fw_get_email_variables($order, $sent_to_admin=false, $plain_text=false, $email=false){
 
