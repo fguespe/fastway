27615e3ae4fae1c88deeae6ad993f779b3034036
fabrizioguespe <fabrizioguespe@MacFabri.local>
2021-10-28 11:50:46 -0300

    new

--- global-templates/modals.php
+++ global-templates/modals.php
@@ -487,18 +487,19 @@ jQuery( ".fw_variations select" ).change(function() {
             
 
             jQuery('#mini_label_stock').remove()
-
+            //Slide!!
+            var ee="clase-"+vara['variation_id']
+            var index = 0;
+            jQuery.each(jQuery(".swiper-wrapper").children(), function(i, item) {
+                if (jQuery(item).hasClass(ee)) {
+                    index = i;
+                    return false;
+                }
+            });
+            swiper.slideTo(index, 500);
+            
             if((vara['is_in_stock'] && vara['is_purchasable']) || vara['backorders_allowed']){
-                //Slide!!
-                var ee="clase-"+vara['variation_id']
-                var index = 0;
-                jQuery.each(jQuery(".swiper-wrapper").children(), function(i, item) {
-                    if (jQuery(item).hasClass(ee)) {
-                        index = i;
-                        return false;
-                    }
-                });
-                swiper.slideTo(index, 500);
+                
                 jQuery('.fw_add_to_cart_button').prop("disabled",false)
             }else {
                 console.log('vara2',vara['is_in_stock'],vara['is_purchasable'],vara['backorders_allowed'])
