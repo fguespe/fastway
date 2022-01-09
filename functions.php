0601247553394a840f26a09b0475ec76525068f1
fabrizioguespe <fabrizioguespe@MacFabri.local>
2022-01-09 12:58:22 -0300

    new

--- functions.php
+++ functions.php
@@ -732,7 +732,7 @@ if ( ! function_exists( 'add_custom_content_meta_box' ) ){
         $args['textarea_rows'] = 6;
         echo '<p class="search-box" style="display:block;float:none">
         <input type="text" name="nama" id="nama" >
-        <input type="button" onclick="buscador()" class="button" value="'+__('Buscar','fastway')+'"></p>
+        <input type="button" onclick="buscador()" class="button" value="'+(__('Buscar','fastway'))+'"></p>
         <script type="text/javascript">
         function buscador(){
           let value=jQuery("#nama").val()
