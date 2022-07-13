bb90b426df7e8b8ed350004073d02f13f35b64cc
fabrizioguespe <fabrizioguespe@MacFabri.local>
2022-07-13 08:26:56 -0300

    new

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -36,7 +36,6 @@ if ( woocommerce_product_loop() ) {
 				if(get_the_title()=='Body splash 30' || get_the_title()=='Guest 2'){
 					echo "2:".get_the_title().' '.$price;
 				}
-				if(!$price)continue;
 			}
 			do_action( 'woocommerce_shop_loop' );
 			wc_get_template_part( 'content', 'product' );
